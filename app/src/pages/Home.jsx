68af676fbb24c7ea1d7c9f35edcc51cf33991920
Curran <curran.kelleher@gmail.com>
2023-02-04 19:20:15 -0500

    Add app package

---
+++ app/src/pages/Home.jsx
@@ -0,0 +1,8 @@
+// Inspired by https://github.com/vitejs/vite-plugin-react/blob/main/playground/ssr-react/src/pages/Home.jsx
+export default function Home() {
+  return (
+    <>
+      <h1>Home</h1>
+    </>
+  );
+}
