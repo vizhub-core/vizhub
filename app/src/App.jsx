e1dd634f0eb0fc6c601b302b517d2ff6e3fd611f
Curran <curran.kelleher@gmail.com>
2023-02-08 18:36:49 -0500

    Add Favicon. Refactor routes

--- app/src/App.jsx
+++ app/src/App.jsx
@@ -1,12 +1,10 @@
-// Inspired by https://github.com/vitejs/vite-plugin-react/blob/main/playground/ssr-react/src/App.jsx
+// Inspired by
+// https://github.com/vitejs/vite-plugin-react/blob/main/playground/ssr-react/src/App.jsx
+// https://github.com/vizhub-core/vizhub/blob/main/vizhub-v2/packages/neoFrontend/src/App.js
 import { Link, Route, Routes } from 'react-router-dom';
 import { About } from './pages/About';
 import { Home } from './pages/Home';
-
-const routes = [
-  { name: 'About', path: '/about', component: About },
-  { name: 'Home', path: '/', component: Home },
-];
+import { Profile } from './pages/Profile';
 
 // TODO bring in this nav
 //const Nav = () => (
@@ -27,9 +25,46 @@ export function App() {
   return (
     <>
       <Routes>
-        {routes.map(({ path, component: RouteComp }) => {
-          return <Route key={path} path={path} element={<RouteComp />}></Route>;
-        })}
+        <Route path="/about" element={<About />} />
+        <Route path="/" element={<Home />} />
+        <Route path="/:userName" element={<Profile />} />
+
+        {/*
+                    <Route path="/:userName/:vizId" component={VizPage} />
+                    <Route path="/404" component={NotFoundPage} />
+                    <Route
+                      path="/authenticated/:provider"
+                      component={AuthPopupPage}
+                    />
+                    <Route path="/authenticated" component={AuthPopupPage} />
+                    <Route exact path="/" component={HomePage} />
+                    <Route path="/auth" component={AuthPage} />
+                    <Route path="/pricing" component={PricingPage} />
+                    <Route
+                      path="/upgrade-success"
+                      component={UpgradeSuccessPage}
+                    />
+                    <Route
+                      path="/upgrade-canceled"
+                      component={UpgradeCanceledPage}
+                    />
+                    <Route path="/terms" component={TermsPage} />
+                    <Route path="/datavis-2020" component={Datavis2020Page} />
+                    <Route path="/contact" component={ContactPage} />
+                    <Route path="/create-viz" component={CreateVizPage} />
+                    <Route path="/search" component={SearchResultsPage} />
+                    <Route path="/stats" component={VizHubStatsPage} />
+                    <Route
+                      path="/creating-viz-from-scratch"
+                      component={CreatingVizFromScratchPage}
+                    />
+                    <Route
+                      path="/:userName/:vizId/forks"
+                      component={ForksPage}
+                    />
+                    <Route path="/:userName/account" component={AccountPage} />
+                    <Route component={NotFoundPage} />
+	  */}
       </Routes>
     </>
   );
--- app/src/pages/About.jsx
+++ app/src/pages/About.jsx
@@ -1,4 +1,4 @@
 // Inspired by https://github.com/vitejs/vite-plugin-react/blob/main/playground/ssr-react/src/pages/Home.jsx
-export function About() {
-  return 'aboyut';
-}
+export const About = () => {
+  return 'About';
+};
