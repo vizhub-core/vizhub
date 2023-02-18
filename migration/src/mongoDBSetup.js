576a57a88441e19f4a6fb0983abbf3a27f51883f
Curran <curran.kelleher@gmail.com>
2023-02-18 08:19:01 +0000

    Got migrations started

--- migration/src/mongoDBSetup.js
+++ migration/src/mongoDBSetup.js
@@ -1,29 +1,37 @@
 import ShareDB from 'sharedb';
+import json1 from 'ot-json1';
 import ShareDBMongo from 'sharedb-mongo';
-import { MongoClient } from 'mongodb-legacy';
+import MongoDB from 'mongodb-legacy';
 import { DatabaseGateways } from 'database';
 
 ShareDB.types.register(json1.type);
+
+const { MongoClient } = MongoDB;
+const v2MongoURI = import.meta.env.VITE_VIZHUB_V2_MONGO_URI;
+const v3MongoURI = import.meta.env.VITE_VIZHUB_V3_MONGO_URI;
 
 // TODO consider unifying this logic between
 // database tests and this file.
 // We'll also need it for the API server.
 export const mongoDBSetup = async () => {
-  const v2MongoURI = import.meta.env.VIZHUB_MONGO_URI;
-  const v3MongoURI = import.meta.env.VIZHUB3_MONGO_URI;
+  console.log(`Connecting to MongoDB`);
+  console.log(`  Using v2 Mongo URI "${v2MongoURI}".`);
+  console.log(`  Using v3 Mongo URI "${v3MongoURI}".`);
 
   const timeout = setTimeout(() => {
     console.log('\nHaving trouble connecting to the database...');
     console.log('  Ensure that the database is running.');
-    console.log(`  Using v2 Mongo URI "${v2MongoURI}".`);
-    console.log(`  Using v3 Mongo URI "${v3MongoURI}".`);
   }, 4000);
 
   const v2MongoClient = new MongoClient(v2MongoURI);
   const v2MongoDBConnection = await v2MongoClient.connect();
   const v2MongoDBDatabase = await v2MongoDBConnection.db();
 
-  const v3MongoClient = new MongoClient(v3MongoURI);
+  const v3MongoClient = new MongoClient(v3MongoURI, {
+    // Specify the AWS DocumentDB cert
+    // See https://us-east-1.console.aws.amazon.com/docdb/home?region=us-east-1#cluster-details/vizhub3proddb
+    tlsCAFile: `rds-combined-ca-bundle.pem`,
+  });
   const v3MongoDBConnection = await v3MongoClient.connect();
   const v3MongoDBDatabase = await v3MongoDBConnection.db();
 
@@ -31,11 +39,11 @@ export const mongoDBSetup = async () => {
     shareDBConnection: new ShareDB({
       db: ShareDBMongo({
         mongo: (callback) => {
-          callback(null, mongoDBConnection);
+          callback(null, v3MongoDBConnection);
         },
       }),
     }).connect(),
-    mongoDBDatabase,
+    mongoDBDatabase: v3MongoDBDatabase,
   });
 
   clearTimeout(timeout);
