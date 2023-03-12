f8cca9198262c4dff8a0c87a8e32c1351b7c8e25
Curran <curran.kelleher@gmail.com>
2023-03-11 21:22:00 -0500

    Add pricing page story

--- ui/src/components/PricingPage/index.jsx
+++ ui/src/components/PricingPage/index.jsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-
-import { Header } from './Header';
+import { Header } from '../Header';
 import './pricing-page.css';
 
 const plans = {
