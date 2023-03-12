d8b07c049415b4cf25a1885efd4e076e6ace259d
Curran <curran.kelleher@gmail.com>
2023-03-11 20:16:58 -0500

    Scaffold components from old code

---
+++ ui/src/components/PricingPage/index.jsx
@@ -0,0 +1,325 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+import { Header } from './Header';
+import './pricing-page.css';
+
+const plans = {
+  free: {
+    name: 'Free',
+    pricing: { price: 0, unit: 'forever' },
+    bullets: [
+      'Public vizzes',
+      'Public folders',
+      'Public collections',
+      'Public organizations',
+      //'Pinned vizzes',
+      //'Upvote vizzes',
+      //"See vizzes you've upvoted",
+      'Search public content',
+      'Comments with mentions',
+      'Interactive code editor',
+      'Continuous hot reloading',
+      'ES modules and JSX support',
+      'Export to standard JavaScript',
+      'Import from CSV and JSON',
+      'Import from other vizzes',
+      //'Automatic code formatting',
+      'Multiplayer editing',
+      'Pull requests',
+      'Revision history',
+      'Social media sharing',
+      'Branded embedding',
+      'Order physical canvas prints',
+      //'Migrate content from bl.ocks.org',
+      'VizHub Forum access',
+    ],
+    callToAction: {
+      text: 'Sign up for free',
+      isOutline: true,
+    },
+  },
+  pro: {
+    name: 'Pro',
+    pricing: { price: 15, unit: 'per user per month' },
+    previousPlanBullet: 'Everything in Free',
+    bullets: [
+      'Private & unlisted vizzes',
+      'Private & unlisted folders',
+      'Private & unlisted collections',
+      'Private organizations',
+      'Search private content',
+      'No-code viz configuration editor',
+      'Programmatically edit files',
+      'Advanced access control',
+      //'Organization analytics',
+      'Password protected sharing',
+      //'Encrypted vizzes',
+      'In-platform meetings',
+      //'Record and live-stream meetings',
+      'GitHub integration',
+      'White-label embedding',
+      'VizHub Pages (microsite hosting)',
+      'Custom domains',
+      'High resolution image export',
+      //'Automatic data updating API',
+    ],
+    callToAction: {
+      text: 'Get started',
+      isOutline: false,
+    },
+  },
+  enterprise: {
+    name: 'Enterprise',
+    pricing: { price: 45, unit: 'per user per month' },
+    previousPlanBullet: 'Everything in Pro',
+    bullets: [
+      'Self hosting',
+      'Single sign-on',
+      'HIPAAcompliance',
+      'FedRAMP compliance',
+      'Dedicated support',
+    ],
+    callToAction: {
+      text: 'Contact us',
+      isOutline: true,
+    },
+  },
+};
+
+const CheckSVG = () => (
+  <svg
+    width="16"
+    height="16"
+    fill="currentColor"
+    viewBox="0 0 16 16"
+  >
+    <path d="M10.97 4.97a.75.75 0 0 1 1.07 1.05l-3.99 4.99a.75.75 0 0 1-1.08.02L4.324 8.384a.75.75 0 1 1 1.06-1.06l2.094 2.093 3.473-4.425a.267.267 0 0 1 .02-.022z" />
+  </svg>
+);
+
+// const ArrowLeftSVG = () => (
+//   <svg
+//     width="16"
+//     height="16"
+//     fill="currentColor"
+//     viewBox="0 0 16 16"
+//   >
+//     <path d="M15 8a.5.5 0 0 0-.5-.5H2.707l3.147-3.146a.5.5 0 1 0-.708-.708l-4 4a.5.5 0 0 0 0 .708l4 4a.5.5 0 0 0 .708-.708L2.707 8.5H14.5A.5.5 0 0 0 15 8z" />
+//   </svg>
+// );
+
+const ArrowLeftShortSVG = () => (
+  <svg
+    width="16"
+    height="16"
+    fill="currentColor"
+    viewBox="0 0 16 16"
+  >
+    <path d="M12 8a.5.5 0 0 1-.5.5H5.707l2.147 2.146a.5.5 0 0 1-.708.708l-3-3a.5.5 0 0 1 0-.708l3-3a.5.5 0 1 1 .708.708L5.707 7.5H11.5a.5.5 0 0 1 .5.5z" />
+  </svg>
+);
+
+const PlanCard = ({
+  plan: {
+    name,
+    previousPlanBullet,
+    bullets,
+    pricing,
+    callToAction,
+  },
+  onClick,
+}) => (
+  <div className="col">
+    <div className="card mb-4 rounded-3">
+      <div className="card-header py-3">
+        <h4 className="my-0 fw-normal">{name}</h4>
+      </div>
+      <div className="card-body">
+        <h1 className="card-title pricing-card-title">
+          ${pricing.price}
+        </h1>
+        {pricing.unit ? (
+          <small className="text-muted">
+            {pricing.unit}
+          </small>
+        ) : null}
+        <ul className="list-unstyled mt-3 mb-4 mx-2 text-start">
+          {previousPlanBullet ? (
+            <li>
+              <span className="mx-2">
+                <ArrowLeftShortSVG />
+              </span>
+              {previousPlanBullet}
+            </li>
+          ) : null}
+          {bullets.map((bullet) => (
+            <li>
+              <span className="mx-2">
+                <CheckSVG />
+              </span>
+              {bullet}
+            </li>
+          ))}
+        </ul>
+        <button
+          type="button"
+          className={`w-100 btn btn-lg ${
+            callToAction.isOutline
+              ? 'btn-outline-primary'
+              : 'btn-primary'
+          }`}
+          onClick={onClick}
+        >
+          {callToAction.text}
+        </button>
+      </div>
+    </div>
+  </div>
+);
+
+export const PricingPage = ({
+  onFreeClick,
+  onProClick,
+  onEnterpriseClick,
+
+  // Header props
+  user,
+  onLogin,
+  onLogout,
+  onCreateViz,
+  onProfile,
+  onForum,
+  onVizHub,
+}) => {
+  return (
+    <>
+      <Header
+        user={user}
+        onLogin={onLogin}
+        onLogout={onLogout}
+        onCreateViz={onCreateViz}
+        onProfile={onProfile}
+        onForum={onForum}
+        onVizHub={onVizHub}
+      />
+      <div className="container py-4 vh-pricing-page">
+        <div className="row row-cols-1 row-cols-lg-3 mb-3 text-center">
+          <PlanCard
+            plan={plans.free}
+            onClick={onFreeClick}
+          />
+          <PlanCard plan={plans.pro} onClick={onProClick} />
+          <PlanCard
+            plan={plans.enterprise}
+            onClick={onEnterpriseClick}
+          />
+        </div>
+        {/*
+      <h2 className="display-6 text-center mb-4">
+        Compare plans
+      </h2>
+
+      <div className="table-responsive">
+        <table className="table text-center">
+          <thead>
+            <tr>
+              <th style={{ width: '34%' }}></th>
+              <th style={{ width: '22%' }}>Free</th>
+              <th style={{ width: '22%' }}>Pro</th>
+              <th style={{ width: '22%' }}>Enterprise</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <th scope="row" className="text-start">
+                Public
+              </th>
+              <td>
+                <CheckSVG />
+              </td>
+              <td>
+                <CheckSVG />
+              </td>
+              <td>
+                <CheckSVG />
+              </td>
+            </tr>
+            <tr>
+              <th scope="row" className="text-start">
+                Private
+              </th>
+              <td></td>
+              <td>
+                <CheckSVG />
+              </td>
+              <td>
+                <CheckSVG />
+              </td>
+            </tr>
+          </tbody>
+
+          <tbody>
+            <tr>
+              <th scope="row" className="text-start">
+                Permissions
+              </th>
+              <td>
+                <CheckSVG />
+              </td>
+              <td>
+                <CheckSVG />
+              </td>
+              <td>
+                <CheckSVG />
+              </td>
+            </tr>
+            <tr>
+              <th scope="row" className="text-start">
+                Sharing
+              </th>
+              <td></td>
+              <td>
+                <CheckSVG />
+              </td>
+              <td>
+                <CheckSVG />
+              </td>
+            </tr>
+            <tr>
+              <th scope="row" className="text-start">
+                Unlimited members
+              </th>
+              <td></td>
+              <td>
+                <CheckSVG />
+              </td>
+              <td>
+                <CheckSVG />
+              </td>
+            </tr>
+            <tr>
+              <th scope="row" className="text-start">
+                Extra security
+              </th>
+              <td></td>
+              <td></td>
+              <td>
+                <CheckSVG />
+              </td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
+      */}
+      </div>
+    </>
+  );
+};
+
+PricingPage.propTypes = {
+  onFreeClick: PropTypes.func.isRequired,
+  onProClick: PropTypes.func.isRequired,
+  onEnterpriseClick: PropTypes.func.isRequired,
+  ...Header.propTypes,
+};
---
+++ ui/src/components/VizPreview/index.jsx
@@ -0,0 +1,52 @@
+import PropTypes from 'prop-types';
+
+// Shows a preview of a viz.
+// See also
+// archive/vizhub-v3-false-start/src/App/VizPreview.js
+export const VizPreview = ({
+  title,
+  thumbnailImageURL,
+  lastUpdatedDateFormatted,
+  ownerName,
+  ownerAvatarURL,
+  onClick,
+}) => {
+  // Verified useMemo optimizations 3/19/22.
+  // To verify, uncomment the following line and make sure re-renders
+  // don't happen for each and every VizPreview when query results change.
+  // console.log('rendering VizPreview');
+
+  return (
+    <div className="viz-preview" onClick={onClick}>
+      <div
+        className="thumbnail"
+        style={{
+          backgroundImage: `url("${thumbnailImageURL}")`,
+        }}
+        alt={title}
+      ></div>
+      <div className="content-container">
+        <div className="last-updated-date">
+          {lastUpdatedDateFormatted}
+        </div>
+        <div className="title">{title}</div>
+      </div>
+      <div className="meta-container">
+        {ownerName ? (
+          <>
+            <img
+              className="owner-avatar-image"
+              src={ownerAvatarURL}
+              alt={ownerName}
+            />
+            <div className="owner-name">{ownerName}</div>
+          </>
+        ) : null}
+      </div>
+    </div>
+  );
+};
+
+VizPreview.propTypes = {
+  onClick: PropTypes.func.isRequired,
+};
