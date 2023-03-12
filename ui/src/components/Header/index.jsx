ec996880b19d5fa0ba7803c8aa43ab76eb8a3209
Curran <curran.kelleher@gmail.com>
2023-03-11 21:16:19 -0500

    Add Header story

---
+++ ui/src/components/Header/index.jsx
@@ -0,0 +1,81 @@
+import { forwardRef } from 'react';
+import PropTypes from 'prop-types';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+import Container from 'react-bootstrap/Container';
+import Dropdown from 'react-bootstrap/Dropdown';
+import Image from 'react-bootstrap/Image';
+import Button from 'react-bootstrap/Button';
+import './header.css';
+
+// Inspired by:
+// https://react-bootstrap.netlify.app/components/dropdowns/#custom-dropdown-components
+const AvatarToggle = forwardRef(({ children, onClick }, ref) => (
+  <button
+    type="button"
+    className="navbar__avatar-toggle dropdown-toggle"
+    ref={ref}
+    onClick={onClick}
+  >
+    {children}
+  </button>
+));
+
+export const Header = ({
+  user,
+  onLogin,
+  onLogout,
+  onCreateViz,
+  onProfile,
+  onForum,
+  onVizHub,
+}) => (
+  <Navbar bg="dark" variant="dark" expand="md">
+    <Container fluid>
+      <Navbar.Brand
+        style={{ cursor: 'pointer' }}
+        onClick={onVizHub}
+      ></Navbar.Brand>
+      <Navbar.Toggle aria-controls="basic-navbar-nav" />
+      <Navbar.Collapse id="basic-navbar-nav">
+        <Nav className="me-auto" />
+        <Nav className="align-items-md-center">
+          <Nav.Link onClick={onForum}>Forum</Nav.Link>
+          {user ? (
+            <Dropdown align="end">
+              <Dropdown.Toggle as={AvatarToggle}>
+                <Image
+                  src="https://github.com/mdo.png"
+                  roundedCircle
+                  width="32"
+                  height="32"
+                />
+              </Dropdown.Toggle>
+              <Dropdown.Menu>
+                <Dropdown.Item onClick={onCreateViz}>Create Viz</Dropdown.Item>
+                <Dropdown.Divider />
+                <Dropdown.Item onClick={onProfile}>Profile</Dropdown.Item>
+                <Dropdown.Divider />
+                <Dropdown.Item onClick={onLogout}>Log out</Dropdown.Item>
+              </Dropdown.Menu>
+            </Dropdown>
+          ) : (
+            <Button size="sm" className="vh-header-button" onClick={onLogin}>
+              Log in
+            </Button>
+          )}
+        </Nav>
+      </Navbar.Collapse>
+    </Container>
+  </Navbar>
+);
+
+Header.propTypes = {
+  user: PropTypes.shape({}),
+  onLogin: PropTypes.func.isRequired,
+  onLogout: PropTypes.func.isRequired,
+  onCreateViz: PropTypes.func.isRequired,
+  onProfile: PropTypes.func.isRequired,
+  onForum: PropTypes.func.isRequired,
+  onVizHub: PropTypes.func.isRequired,
+};
--- ui/src/stories/Header.stories.js
+++ ui/src/stories/Header.stories.js
@@ -1,4 +1,4 @@
-import { Header } from './Header';
+import { Header } from '../components/Header';
 
 export default {
   title: 'Example/Header',
