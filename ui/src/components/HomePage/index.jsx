799aabfe5e412a5ccaee1d6a4319e2c772e9b896
Curran <curran.kelleher@gmail.com>
2023-03-26 06:22:14 -0400

    Landing page first pass

--- ui/src/components/HomePage/index.jsx
+++ ui/src/components/HomePage/index.jsx
@@ -1,3 +1,10 @@
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+import InputGroup from 'react-bootstrap/InputGroup';
+import Form from 'react-bootstrap/Form';
+import Button from 'react-bootstrap/Button';
+
 import { Header } from '../Header';
 import '../index.scss';
 import './home-page.scss';
@@ -6,7 +13,21 @@ export const HomePage = (props) => {
   return (
     <div className="vh-page vh-home-page">
       <Header {...props} />
-      Join the waitlist
+      <div className="blurb-container">
+        <div className="blurb">
+          <div className="callout">Accelerate Dataviz Delivery</div>
+          <p className="description">
+            <strong>Develop bespoke data visualizations</strong> faster and more
+            collaboratively than ever with VizHub 3. Built for client services.
+          </p>
+          <InputGroup className="vh-email-submission">
+            <Form.Control placeholder="Your email" aria-label="Your email" />
+            <Button variant="primary" id="button-addon2">
+              Join the private beta
+            </Button>
+          </InputGroup>
+        </div>
+      </div>
     </div>
   );
 };
