cc4f1e4d2647a4d222e145e0c1971f0106eff2c5
Curran <curran.kelleher@gmail.com>
2023-03-26 05:44:17 -0400

    Solve full height page issue

--- ui/src/components/HomePage/index.jsx
+++ ui/src/components/HomePage/index.jsx
@@ -1,25 +1,12 @@
-import Container from 'react-bootstrap/Container';
-import PropTypes from 'prop-types';
-import Button from 'react-bootstrap/Button';
 import { Header } from '../Header';
-import { VizPreviewCollection } from '../VizPreviewCollection';
 import '../index.scss';
 import './home-page.scss';
 
 export const HomePage = (props) => {
-  const { renderVizPreviews, onMoreClick } = props;
   return (
-    <div className="home-page">
+    <div className="vh-page vh-home-page">
       <Header {...props} />
-      <Container className="mt-3 mb-3">
-        <VizPreviewCollection>{renderVizPreviews()}</VizPreviewCollection>
-        <div className="mt-3 mb-3 d-flex justify-content-center">
-          <Button onClick={onMoreClick}>More</Button>
-        </div>
-      </Container>
+      Join the waitlist
     </div>
   );
 };
-
-// Just for Storybook event listeners
-HomePage.propTypes = { ...Header.propTypes, onMoreClick: PropTypes.func };
