77baec43ba8ce6b2b0f5affc2d03f5604a140ca2
Curran <curran.kelleher@gmail.com>
2023-03-27 09:24:03 -0400

    Fake storybook

--- components/src/App.jsx
+++ components/src/App.jsx
@@ -1,11 +1,32 @@
 import { useState } from 'react';
 import './App.scss';
 
+// Auto generates routes from files under ./pages
+// https://vitejs.dev/guide/features.html#glob-import
+const stories = import.meta.glob('./stories/*.jsx', { eager: true });
+
+const entries = Object.keys(stories).map((key) => {
+  const name = key.substring(10).split('.')[0];
+  const component = stories[key][name];
+  return { name, component };
+});
+
 function App() {
+  const [Component, setComponent] = useState(null);
   return (
     <div className="App">
-      <div className="sidebar">Sidebar</div>
-      <div className="component">Component</div>
+      <div className="sidebar">
+        {entries.map(({ name, component }) => (
+          <div
+            key={name}
+            className="entry"
+            onClick={() => setComponent(component)}
+          >
+            {name}
+          </div>
+        ))}
+      </div>
+      <div className="component">{Component}</div>
     </div>
   );
 }
---
+++ components/src/stories/Other.jsx
@@ -0,0 +1,3 @@
+export const Other = () => {
+  return <div>Other</div>;
+};
