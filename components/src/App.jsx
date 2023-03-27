7314553801f0acca33c0997a85ed64c04f2a2e83
Curran <curran.kelleher@gmail.com>
2023-03-27 08:43:24 -0400

    Begin migrating away from Storybook

---
+++ components/src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from 'react';
+import './App.scss';
+
+function App() {
+  return (
+    <div className="App">
+      <div className="sidebar">Sidebar</div>
+      <div className="component">Component</div>
+    </div>
+  );
+}
+
+export default App;
