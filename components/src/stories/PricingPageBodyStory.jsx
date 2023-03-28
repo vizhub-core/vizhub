5508bfb4dd8878b2ed43d852084dc4fbb1f4ca91
Curran <curran.kelleher@gmail.com>
2023-03-27 23:06:09 -0400

    Add Pricing Page Story

---
+++ components/src/stories/PricingPageBodyStory.jsx
@@ -0,0 +1,11 @@
+import { PricingPageBody } from '../components/PricingPageBody';
+
+const Story = () => {
+  return (
+    <div className="layout-fullscreen">
+      <PricingPageBody />
+    </div>
+  );
+};
+
+export default Story;
