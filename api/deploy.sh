7a3dc98a47d739cf348cb223486854f1a74f9c6e
Curran <curran.kelleher@gmail.com>
2023-03-29 08:22:29 -0400

    Hello world for API

--- api/deploy.sh
+++
@@ -1,4 +0,0 @@
-git pull
-docker build -t vizhub3 .
-docker tag vizhub3:latest 638829852636.dkr.ecr.us-east-1.amazonaws.com/vizhub3:latest
-docker push 638829852636.dkr.ecr.us-east-1.amazonaws.com/vizhub3:latest
--- api/src/entry-server.js
+++ api/src/entry-server.js
@@ -2,7 +2,10 @@
 // https://github.com/curran/sharedb-racer-react-demo/blob/main/src/server.js
 // https://github.com/vizhub-core/vizhub/blob/main/prototypes/open-core-first-attempt/packages/vizhub-core/src/server/index.js
 // https://github.com/vizhub-core/vizhub/blob/main/vizhub-v3/vizhub-app/src/server/index.js
-import http from 'http';
-import express from 'express';
 
 // TODO pull in MongoDB + ShareDB setup from database package
+export const api = (expressApp) => {
+  expressApp.get('/api/test', (req, res) => {
+    res.send('Hello World!');
+  });
+};
