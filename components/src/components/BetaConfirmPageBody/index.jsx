4f923c8d77928c48a42f74040ff7e43b0fa236e3
Curran <curran.kelleher@gmail.com>
2023-03-29 07:50:31 -0400

    Create beta confirm page

---
+++ components/src/components/BetaConfirmPageBody/index.jsx
@@ -0,0 +1,18 @@
+import 'vizhub-ui/dist/vizhub-ui.css';
+import '../index.scss';
+import './styles.scss';
+
+export const BetaConfirmPageBody = ({ onEmailSubmit }) => {
+  return (
+    <div className="vh-page vh-beta-confirm-page">
+      <div className="blurb-container">
+        <div className="blurb">
+          <div className="callout large">Thanks!</div>
+          <p className="description">
+            Thanks for your interest! You'll be hearing from us.
+          </p>
+        </div>
+      </div>
+    </div>
+  );
+};
---
+++ components/src/stories/BetaConfirmPageBodyStory.jsx
@@ -0,0 +1,11 @@
+import { BetaConfirmPageBody } from '../components/BetaConfirmPageBody';
+
+const Story = () => {
+  return (
+    <div className="layout-fullscreen">
+      <BetaConfirmPageBody />
+    </div>
+  );
+};
+
+export default Story;
