3c992f2454cf8d018bf5689d4bcdee6c4ec90337
Curran <curran.kelleher@gmail.com>
2023-04-17 02:22:05 -0400

    Add health check route

--- api/src/endpoints/index.js
+++ api/src/endpoints/index.js
@@ -1,5 +1,10 @@
+import { healthCheck } from './healthCheck';
 import { privateBetaEmailSubmit } from './privateBetaEmailSubmit';
 import { recordAnalyticsEvents } from './recordAnalyticsEvents';
 
 // TODO Glob import
-export const endpoints = [privateBetaEmailSubmit, recordAnalyticsEvents];
+export const endpoints = [
+  healthCheck,
+  privateBetaEmailSubmit,
+  recordAnalyticsEvents,
+];
