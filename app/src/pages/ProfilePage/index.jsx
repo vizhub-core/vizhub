19b9aa1dce0cafdfc632ed1acf1a906b3aea7d91
Curran <curran.kelleher@gmail.com>
2023-04-18 09:36:31 -0400

    Subscribe for profile updates

--- app/src/pages/ProfilePage/index.jsx
+++ app/src/pages/ProfilePage/index.jsx
@@ -1,11 +1,11 @@
 import { ProfilePageBody } from 'components/src/components/ProfilePageBody';
+import { useShareDBDocData } from '../../useShareDBDocData';
 // Inspired by https://github.com/vitejs/vite-plugin-react/blob/main/playground/ssr-react/src/pages/Home.jsx
 export const ProfilePage = ({ pageData }) => {
   const { profileUserSnapshot } = pageData;
 
-  // TODO ShareDB sync
-  const profileUser = profileUserSnapshot.data;
-
+  // Subscribe to real-time updates in case something changes like display name.
+  const profileUser = useShareDBDocData(profileUserSnapshot, 'User');
   const { userName, displayName, picture } = profileUser;
 
   return (
