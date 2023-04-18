e0391d417cfb9cea158f3981d14ebda1338e94a7
Curran <curran.kelleher@gmail.com>
2023-04-18 09:28:05 -0400

    Refactor ShareDB subscription logic

--- app/src/pages/SandboxPage/EventVizLive.jsx
+++ app/src/pages/SandboxPage/EventVizLive.jsx
@@ -1,59 +1,11 @@
-import { useState, useEffect } from 'react';
-import ShareDBClient from 'sharedb-client-browser/dist/sharedb-client-umd.cjs';
-import { otType } from 'ot';
-import { toCollectionName } from 'database/src/toCollectionName';
 import { EventViz } from './EventViz';
-
-// Register our custom JSON1 OT type that supports presence.
-// See https://github.com/vizhub-core/json1-presence
-ShareDBClient.types.register(otType);
-
-// Establish the singleton ShareDB connection over WebSockets.
-// TODO consider using reconnecting WebSocket
-const { Connection } = ShareDBClient;
-
-const getConnection = (() => {
-  let connection;
-  return () => {
-    if (!connection) {
-      const wsProtocol =
-        window.location.protocol === 'https:' ? 'wss://' : 'ws://';
-      const socket = new WebSocket(wsProtocol + window.location.host);
-      connection = new Connection(socket);
-    }
-
-    return connection;
-  };
-})();
-
-const logShareDBError = (error) => {
-  if (error) console.log(error);
-};
+import { useShareDBDocData } from '../../useShareDBDocData';
 
 export const EventVizLive = ({ analyticsEventSnapshot, vizModule, title }) => {
-  const [analyticsEvent, setAnalyticsEvent] = useState(
-    analyticsEventSnapshot.data
+  const analyticsEvent = useShareDBDocData(
+    analyticsEventSnapshot,
+    'AnalyticsEvent'
   );
-
-  useEffect(() => {
-    const connection = getConnection();
-    const shareDBDoc = connection.get(
-      toCollectionName('AnalyticsEvent'),
-      analyticsEventSnapshot.data.id
-    );
-    shareDBDoc.ingestSnapshot(analyticsEventSnapshot, logShareDBError);
-    shareDBDoc.subscribe(logShareDBError);
-
-    const updateState = () => {
-      setAnalyticsEvent(shareDBDoc.data);
-    };
-
-    shareDBDoc.on('op batch', updateState);
-
-    return () => {
-      shareDBDoc.off('op batch', updateState);
-    };
-  }, []);
 
   return (
     <EventViz
---
+++ app/src/useShareDBDocData.js
@@ -0,0 +1,59 @@
+import { useState, useEffect } from 'react';
+import ShareDBClient from 'sharedb-client-browser/dist/sharedb-client-umd.cjs';
+import { otType } from 'ot';
+import { toCollectionName } from 'database/src/toCollectionName';
+
+// Register our custom JSON1 OT type that supports presence.
+// See https://github.com/vizhub-core/json1-presence
+ShareDBClient.types.register(otType);
+
+const { Connection } = ShareDBClient;
+
+const getConnection = (() => {
+  // Singleton ShareDB connection over WebSockets.
+  let connection;
+  return () => {
+    if (!connection) {
+      const wsProtocol =
+        window.location.protocol === 'https:' ? 'wss://' : 'ws://';
+      // TODO consider using reconnecting WebSocket
+      const socket = new WebSocket(wsProtocol + window.location.host);
+      connection = new Connection(socket);
+    }
+
+    return connection;
+  };
+})();
+
+const logShareDBError = (error) => {
+  if (error) console.log(error);
+};
+
+// Subscribes to live updates via ShareDB.
+export const useShareDBDocData = (snapshot, entityName) => {
+  const [data, setData] = useState(snapshot.data);
+
+  useEffect(() => {
+    const connection = getConnection();
+    const shareDBDoc = connection.get(
+      toCollectionName(entityName),
+      snapshot.data.id
+    );
+    shareDBDoc.ingestSnapshot(snapshot, logShareDBError);
+    shareDBDoc.subscribe(logShareDBError);
+
+    const updateState = () => {
+      setData(shareDBDoc.data);
+    };
+
+    shareDBDoc.on('op batch', updateState);
+
+    // TODO test cleanup for leaks
+    // use doc.unsubscribe? doc.destroy?
+    return () => {
+      shareDBDoc.off('op batch', updateState);
+    };
+  }, []);
+
+  return data;
+};
