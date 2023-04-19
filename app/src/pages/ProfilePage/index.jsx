9485a12a478496ea2b4fc131d6142446dd59d520
Curran <curran.kelleher@gmail.com>
2023-04-18 22:39:14 -0400

    List viz infos in profile page

--- app/src/pages/ProfilePage/index.jsx
+++ app/src/pages/ProfilePage/index.jsx
@@ -2,7 +2,7 @@ import { ProfilePageBody } from 'components/src/components/ProfilePageBody';
 import { useShareDBDocData } from '../../useShareDBDocData';
 // Inspired by https://github.com/vitejs/vite-plugin-react/blob/main/playground/ssr-react/src/pages/Home.jsx
 export const ProfilePage = ({ pageData }) => {
-  const { profileUserSnapshot } = pageData;
+  const { profileUserSnapshot, infoSnapshots } = pageData;
 
   // Subscribe to real-time updates in case something changes like display name.
   const profileUser = useShareDBDocData(profileUserSnapshot, 'User');
@@ -10,7 +10,11 @@ export const ProfilePage = ({ pageData }) => {
 
   return (
     <ProfilePageBody
-      renderVizPreviews={() => null}
+      renderVizPreviews={() =>
+        infoSnapshots.map((snapshot) => (
+          <div key={snapshot.id}>{snapshot.data.title}</div>
+        ))
+      }
       displayName={displayName}
       userName={userName}
       picture={picture}
--- app/src/pages/ProfilePage/server.js
+++ app/src/pages/ProfilePage/server.js
@@ -2,15 +2,28 @@ import { ProfilePage } from './index';
 
 ProfilePage.getPageData = async ({ gateways, params }) => {
   const { userName } = params;
-  const { getUserByUserName } = gateways;
+  const { getUserByUserName, getInfosByOwner } = gateways;
 
-  const result = await getUserByUserName(userName);
-  if (result.outcome === 'success') {
-    // getInfosByOwner
+  const userResult = await getUserByUserName(userName);
+  if (userResult.outcome === 'success') {
+    const profileUserSnapshot = userResult.value;
+
+    const owner = profileUserSnapshot.data.id;
+
+    let infoSnapshots;
+    const infoSnapshotsResult = await getInfosByOwner(owner);
+    if (infoSnapshotsResult.outcome === 'success') {
+      infoSnapshots = infoSnapshotsResult.value;
+    } else {
+      infoSnapshots = [];
+      console.log('Error when fetching infos by owner:');
+      console.log(infoSnapshotsResult.error);
+    }
 
     return {
       title: `${userName} on VizHub`,
-      profileUserSnapshot: result.value,
+      profileUserSnapshot,
+      infoSnapshots,
     };
   }
 
