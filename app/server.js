61426e2edcea4984995e5fb0006f67c15515d09a
Curran Kelleher <curran.kelleher@gmail.com>
2023-06-07 17:25:21 -0400

    Adopt Sentry. Closes #32

--- app/server.js
+++ app/server.js
@@ -12,6 +12,7 @@ import jsesc from 'jsesc';
 import { WebSocketServer } from 'ws';
 import WebSocketJSONStream from '@teamwork/websocket-json-stream';
 import { matchPath } from 'react-router-dom';
+import * as Sentry from '@sentry/node';
 import { seoMetaTags } from './seoMetaTags.js';
 
 const env = process.env;
@@ -42,6 +43,31 @@ async function createServer(
 
   // Support parsing of JSON bodies in API requests.
   app.use(express.json());
+
+  // Set up Sentry.
+  // See https://vizhub.sentry.io/onboarding/setup-docs/
+  Sentry.init({
+    dsn: 'https://645705f71cac4ca08b3714784eb530f0@o4505320347271168.ingest.sentry.io/4505320348581888',
+    integrations: [
+      // enable HTTP calls tracing
+      new Sentry.Integrations.Http({ tracing: true }),
+      // enable Express.js middleware tracing
+      new Sentry.Integrations.Express({ app }),
+      // Automatically instrument Node.js libraries and frameworks
+      ...Sentry.autoDiscoverNodePerformanceMonitoringIntegrations(),
+    ],
+
+    // Set tracesSampleRate to 1.0 to capture 100%
+    // of transactions for performance monitoring.
+    // We recommend adjusting this value in production
+    tracesSampleRate: 1.0,
+  });
+
+  // RequestHandler creates a separate execution context, so that all
+  // transactions/spans/breadcrumbs are isolated across requests
+  app.use(Sentry.Handlers.requestHandler());
+  // TracingHandler creates a trace for every incoming request
+  app.use(Sentry.Handlers.tracingHandler());
 
   let vite;
   if (!isProd) {
@@ -210,6 +236,9 @@ async function createServer(
       res.status(500).end(e.stack);
     }
   });
+
+  // The error handler must be before any other error middleware and after all controllers
+  app.use(Sentry.Handlers.errorHandler());
 
   server.listen(5173, () => {
     console.log('VizHub App Server listening at http://localhost:5173');
