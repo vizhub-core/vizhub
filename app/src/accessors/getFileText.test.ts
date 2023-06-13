7d5287b1bf2d7249a9b5d62c87aadb9ab6ff5057
Curran Kelleher <curran.kelleher@gmail.com>
2023-06-13 15:18:42 -0400

    Add license accessor

--- app/src/accessors/getFileText.test.ts
+++ app/src/accessors/getFileText.test.ts
@@ -1,9 +1,7 @@
-// TODO https://github.com/vizhub-core/vizhub3/issues/50
 import { describe, it, expect } from 'vitest';
 import { getFileText } from './getFileText';
 import { primordialViz } from 'gateways/test/fixtures';
-
-// The two tests marked with concurrent will be run in parallel
+import { Content } from 'entities';
 describe('getFileText', () => {
   it('should return null if file is missing', () => {
     expect(getFileText(primordialViz.content, 'nonexistent.js')).toBe(null);
@@ -12,7 +10,7 @@ describe('getFileText', () => {
     expect(getFileText(null, 'nonexistent.js')).toBe(null);
   });
   it('should return null if content is missing files', () => {
-    expect(getFileText({}, 'nonexistent.js')).toBe(null);
+    expect(getFileText({} as Content, 'nonexistent.js')).toBe(null);
   });
 
   it('should return text for a given file', () => {
---
+++ app/src/accessors/getLicense.test.ts
@@ -0,0 +1,47 @@
+import { describe, it, expect } from 'vitest';
+import { getLicense, defaultLicense } from './getLicense';
+import { primordialViz } from 'gateways/test/fixtures';
+import { Content } from 'entities';
+
+// The two tests marked with concurrent will be run in parallel
+describe('getLicense', () => {
+  it('should return default license if file is missing', () => {
+    expect(getLicense(primordialViz.content)).toBe(defaultLicense);
+  });
+  it('should return default license if content is missing', () => {
+    expect(getLicense(null)).toBe(defaultLicense);
+  });
+  it('should return default license if content is missing files', () => {
+    expect(getLicense({} as Content)).toBe(defaultLicense);
+  });
+
+  it('should return specified license', () => {
+    expect(
+      getLicense({
+        ...primordialViz.content,
+        files: {
+          ...primordialViz.content.files,
+          '9693462': {
+            name: 'package.json',
+            text: '{"license": "GPL-3.0"}',
+          },
+        },
+      })
+    ).toBe('GPL-3.0');
+  });
+
+  it('should return default license if package.json is invalid JSON', () => {
+    expect(
+      getLicense({
+        ...primordialViz.content,
+        files: {
+          ...primordialViz.content.files,
+          '9693462': {
+            name: 'package.json',
+            text: '{"license": "GPL',
+          },
+        },
+      })
+    ).toBe(defaultLicense);
+  });
+});
