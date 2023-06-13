eede8409c6e7c86172d69068c72c1ee5c07567c8
Curran <curran.kelleher@gmail.com>
2023-06-13 14:27:33 -0400

    Add star to viz viewer

--- components/src/components/UpvoteWidget/index.jsx
+++ components/src/components/UpvoteWidget/index.jsx
@@ -3,7 +3,7 @@ import './styles.css';
 
 export const UpvoteWidget = ({ upvotesCount }) => (
   <div className="vh-upvote-widget">
-    <StarSVG height={20} />
+    <StarSVG height={22} />
     {upvotesCount}
   </div>
 );
