4f795741952d59d9f84faf25fba10ce69f924f2f
Curran Kelleher <curran.kelleher@gmail.com>
2023-06-13 14:43:19 -0400

    Adopt Vitest, add getFileText test

--- app/src/accessors/getFileText.test.ts
+++ app/src/accessors/getFileText.test.ts
@@ -1,1 +1,23 @@
 // TODO https://github.com/vizhub-core/vizhub3/issues/50
+import { describe, it, expect } from 'vitest';
+import { getFileText } from './getFileText';
+import { primordialViz } from 'gateways/test/fixtures';
+
+// The two tests marked with concurrent will be run in parallel
+describe('getFileText', () => {
+  it('should return null if file is missing', () => {
+    expect(getFileText(primordialViz.content, 'nonexistent.js')).toBe(null);
+  });
+  it('should return null if content is missing', () => {
+    expect(getFileText(null, 'nonexistent.js')).toBe(null);
+  });
+  it('should return null if content is missing files', () => {
+    expect(getFileText({}, 'nonexistent.js')).toBe(null);
+  });
+
+  it('should return text for a given file', () => {
+    expect(getFileText(primordialViz.content, 'index.html')).toBe(
+      '<body>Hello</body>'
+    );
+  });
+});
