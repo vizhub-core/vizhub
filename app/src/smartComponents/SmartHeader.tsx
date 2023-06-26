bd19bd6d524e8034e625bf3bee673ac46ca06cd7
Curran Kelleher <curran.kelleher@gmail.com>
2023-06-26 15:52:31 -0400

    Navigate to the home page when the user clicks on the VizHub logo. Closes #135

--- app/src/smartComponents/SmartHeader.tsx
+++ app/src/smartComponents/SmartHeader.tsx
@@ -4,12 +4,19 @@ import { AuthenticatedUserContext } from '../contexts/AuthenticatedUserContext';
 
 export const SmartHeader = () => {
   const authenticatedUser = useContext(AuthenticatedUserContext);
+
+  // Navigate to the home page when the user clicks on the VizHub logo.
+  const handleVizHubClick = () => {
+    window.location.href = '/';
+  };
+
   return (
     <Header
       loginHref={'/login'}
       logoutHref={'/logout'}
       profileHref={`/${authenticatedUser?.userName}`}
       authenticatedUserAvatarURL={authenticatedUser?.picture}
+      onVizHubClick={handleVizHubClick}
     />
   );
 };
