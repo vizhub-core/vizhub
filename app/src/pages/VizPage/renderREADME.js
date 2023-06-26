ef2e7825fc3c72804e685547cd5a3fbd951913ce
Curran Kelleher <curran.kelleher@gmail.com>
2023-06-26 11:27:24 -0400

    Style YouTube embeds

--- app/src/pages/VizPage/renderREADME.js
+++ app/src/pages/VizPage/renderREADME.js
@@ -1,10 +1,27 @@
+// Wraps YouTube embeds in a styled wrapper,
+// so that the embed resizes nicely,
+// preserving aspect ratio.
+const responsiveYouTube = (html) =>
+  html.replace(
+    /<iframe(.+)youtube(.+)<\/iframe>/g,
+    (match) => `<div class='responsive-youtube'>${match}</div>`
+  );
+
 export const renderREADME = (readmeMarkdown, renderMarkdown, xss) =>
   readmeMarkdown
-    ? xss(renderMarkdown.parse(readmeMarkdown), {
-        // Allow iframes in READMEs
-        whiteList: {
-          ...xss.whiteList,
-          iframe: ['src', 'width', 'height', 'frameborder', 'allowfullscreen'],
-        },
-      })
+    ? responsiveYouTube(
+        xss(renderMarkdown.parse(readmeMarkdown), {
+          // Allow iframes in READMEs
+          whiteList: {
+            ...xss.whiteList,
+            iframe: [
+              'src',
+              'width',
+              'height',
+              'frameborder',
+              'allowfullscreen',
+            ],
+          },
+        })
+      )
     : '';
