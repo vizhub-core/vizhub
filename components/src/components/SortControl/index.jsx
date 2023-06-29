ebc45714fd8597b495e1cfce978ea29870f2de3e
Curran Kelleher <curran.kelleher@gmail.com>
2023-06-29 09:24:55 -0400

    Add sort control to profile page

--- components/src/components/SortControl/index.jsx
+++ components/src/components/SortControl/index.jsx
@@ -4,19 +4,26 @@ import { Form, Dropdown, Row, Col } from '../bootstrap';
 export const SortControl = ({ sortId, setSortId, sortOptions }) => {
   // Possible sorts that are not the current sort.
   const otherSortOptions = useMemo(
-    () => sortOptions.filter(({ id }) => id !== sortId),
+    () => (sortOptions ? sortOptions.filter(({ id }) => id !== sortId) : []),
     [sortOptions, sortId]
   );
 
   // Used for looking up a sort label by id.
   const sortsById = useMemo(
-    () => new Map(sortOptions.map((sortOption) => [sortOption.id, sortOption])),
+    () =>
+      new Map(
+        sortOptions
+          ? sortOptions.map((sortOption) => [sortOption.id, sortOption])
+          : []
+      ),
     [sortOptions]
   );
 
   return (
     <Form.Group className="mb-3" controlId="sort" as={Row}>
-      <Form.Label column>Sort by</Form.Label>
+      <Form.Label column sm="auto">
+        Sort by
+      </Form.Label>
       <Col className="px-0">
         <Dropdown onSelect={setSortId}>
           <Dropdown.Toggle id="dropdown-sort">
--- components/src/stories/ProfilePageBodyStory.jsx
+++ components/src/stories/ProfilePageBodyStory.jsx
@@ -1,16 +1,29 @@
+import { useState } from 'react';
 import { ProfilePageBody } from '../components/ProfilePageBody';
 import { renderVizPreviews } from './renderVizPreviews';
+import { args as sortControlArgs } from './SortControlStory';
 
 const args = {
   userName: 'schmoe',
   displayName: 'Joe Schmoe',
   renderVizPreviews,
+  onMoreClick: () => {
+    console.log('More clicked');
+  },
+  picture: 'https://avatars.githubusercontent.com/u/98681?v=4',
 };
 
 const Story = () => {
+  const [sortId, setSortId] = useState(sortControlArgs.initialSortId);
+
   return (
     <div className="layout-fullscreen">
-      <ProfilePageBody {...args} />
+      <ProfilePageBody
+        {...args}
+        sortId={sortId}
+        setSortId={setSortId}
+        sortOptions={sortControlArgs.sortOptions}
+      />
     </div>
   );
 };
