40ae551ad0587a1e6a42366ca235e0df8840b9ae
Curran Kelleher <curran.kelleher@gmail.com>
2023-07-01 12:15:08 -0400

    Got page to load with query param

--- app/src/contexts/SortContext.tsx
+++ app/src/contexts/SortContext.tsx
@@ -1,4 +1,5 @@
 import { createContext, useEffect, useMemo, useState } from 'react';
+import { useLocation } from 'react-router-dom';
 import { SortId, defaultSortOption } from 'entities';
 
 // This context provides the current sortId and a function to change it.
@@ -11,6 +12,10 @@ export const SortContext = createContext<{
 export const SortProvider = ({ children }) => {
   // TODO URL param for sort
   const [sortId, setSortId] = useState(defaultSortOption.id);
+
+  const location = useLocation();
+
+  console.log('location', location);
 
   useEffect(() => {
     console.log('sortId', sortId);
