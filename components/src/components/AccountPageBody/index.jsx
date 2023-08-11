2e643e561882ddaee952e1e7707162d950c1b246
Curran <curran.kelleher@gmail.com>
2023-08-11 17:27:49 -0400

    Update account page copy

--- components/src/components/AccountPageBody/index.jsx
+++ components/src/components/AccountPageBody/index.jsx
@@ -12,8 +12,8 @@ export const AccountPageBody = ({
   return (
     <div className="vh-page vh-account-page">
       <div className="px-4 py-3">
-        <div className="d-flex flex-column">
-          <h1>Your Account</h1>
+        <div className="d-flex flex-column mt-2">
+          <h1>Account</h1>
           {!isUserAuthenticated ? (
             <div>
               <p>You are not logged in.</p>
