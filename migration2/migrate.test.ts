6601b5b10724e49582ee7af0009072b45a74074b
Curran <curran.kelleher@gmail.com>
2023-09-13 14:27:28 -0400

    Start on proper migration

--- migration2/migrate.test.ts
+++ migration2/migrate.test.ts
@@ -1,12 +1,41 @@
 // See also
 import { describe, it, expect } from 'vitest';
-import { Result } from 'gateways';
 import { migrate, MigrateResult } from './migrate';
 
-describe('migrate knows it is in test mode', async () => {
-  it('getViz, success case', async () => {
+describe('migrate', async () => {
+  it('should know it is in test mode', async () => {
     const migrateResult: MigrateResult = await migrate({
       isTest: true,
+    });
+
+    expect(migrateResult.isTestRun).toEqual(true);
+  });
+  it('should query for vizzes in the current batch', async () => {
+    // Hardcoded earliest timestamp.
+    // This is the lowest value for `vizInfo.createdTimestamp` in the V2 database.
+    const firstVizCreationDate =
+      timestampToDate(1534246611);
+
+    // Floor the week using d3-time
+    const firstVizCreationDateFloored = timeWeek.floor(
+      firstVizCreationDate,
+    );
+    // Define a one-week batch of vizzes to migrate.
+    const startTimeDate = timeWeek.offset(
+      firstVizCreationDateFloored,
+      batchNumber,
+    );
+    const endTimeDate = timeWeek.offset(startTimeDate, 1);
+
+    const startTime = dateToTimestamp(startTimeDate);
+    const endTime = dateToTimestamp(endTimeDate);
+
+    const migrateResult: MigrateResult = await migrate({
+      isTest: true,
+      iterateVizzes: {
+        batchStartTimestamp,
+        batchEndTimestamp,
+      },
     });
 
     expect(migrateResult.isTestRun).toEqual(true);
