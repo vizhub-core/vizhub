b620a5b1b2fab41d64bc188bf57da26f7cabef58
Curran <curran.kelleher@gmail.com>
2023-09-29 13:45:49 -0400

    More embeddings tests

--- gateways/test/embeddingsTest.ts
+++ gateways/test/embeddingsTest.ts
@@ -35,7 +35,7 @@ export const embeddingsTest = () => {
       );
     });
 
-    it('knnVizEmbeddingSearch', async () => {
+    it('knnVizEmbeddingSearch - basic case', async () => {
       const gateways = await initGateways();
       const { saveVizEmbedding } = gateways;
 
@@ -53,6 +53,59 @@ export const embeddingsTest = () => {
       assert(searchResult.outcome === 'success');
       expect(searchResult.value).toEqual([
         sampleVizEmbedding.vizId,
+      ]);
+    });
+
+    it('knnVizEmbeddingSearch - many vectors case', async () => {
+      const gateways = await initGateways();
+      const { saveVizEmbedding } = gateways;
+
+      assert(
+        (await saveVizEmbedding(sampleVizEmbedding))
+          .outcome === 'success',
+      );
+
+      assert(
+        (
+          await saveVizEmbedding({
+            ...sampleVizEmbedding,
+            vizId: 'viz2',
+            embedding: [0.5, 0.5, 0.5],
+          })
+        ).outcome === 'success',
+      );
+
+      assert(
+        (
+          await saveVizEmbedding({
+            ...sampleVizEmbedding,
+            vizId: 'viz3',
+            embedding: [0.1, 0.1, 0.1],
+          })
+        ).outcome === 'success',
+      );
+
+      assert(
+        (
+          await saveVizEmbedding({
+            ...sampleVizEmbedding,
+            vizId: 'viz4',
+            embedding: [0.9, 0.9, 0.9],
+          })
+        ).outcome === 'success',
+      );
+
+      const searchResult =
+        await gateways.knnVizEmbeddingSearch(
+          sampleVizEmbedding.embedding,
+          3,
+        );
+
+      assert(searchResult.outcome === 'success');
+      expect(searchResult.value).toEqual([
+        'viz1',
+        'viz4',
+        'viz2',
       ]);
     });
   });
