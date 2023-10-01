3c1a29cc10232452276ec929133c4218c732f508
Curran <curran.kelleher@gmail.com>
2023-10-01 10:31:15 -0400

    Add more checks to validateViz

--- interactors/src/validateViz.ts
+++ interactors/src/validateViz.ts
@@ -1,27 +1,80 @@
 import { Gateways, Result, ok } from 'gateways';
-import { VizId } from 'entities';
+import { Commit, VizId } from 'entities';
+import { GetContentAtCommit } from './getContentAtCommit';
+import equal from 'deep-equal';
 
 // validateViz
 // * Validates a viz
 export const ValidateViz =
-  ({ getInfo, getContent }: Gateways) =>
+  (gateways: Gateways) =>
   async (id: VizId): Promise<Result<'success'>> => {
-    const infoResult = await getInfo(id);
+    const { getInfo, getContent, getCommit } = gateways;
+    const getContentAtCommit = GetContentAtCommit(gateways);
 
+    const infoResult = await getInfo(id);
     if (infoResult.outcome === 'failure') {
       return infoResult;
     }
 
     const contentResult = await getContent(id);
-
     if (contentResult.outcome === 'failure') {
       return contentResult;
     }
 
+    const info = infoResult.value.data;
+    const content = contentResult.value.data;
+
+    const startResult = await getCommit(info.start);
+    if (startResult.outcome === 'failure') {
+      return startResult;
+    }
+
+    const endResult = await getCommit(info.end);
+    if (endResult.outcome === 'failure') {
+      return endResult;
+    }
+
+    const startCommit: Commit = startResult.value;
+    const endCommit: Commit = endResult.value;
+
+    if (startCommit.timestamp !== info.created) {
+      throw new Error(
+        'Invariant: `startCommit.timestamp === info.created`',
+      );
+    }
+
+    if (endCommit.timestamp !== info.updated) {
+      throw new Error(
+        'Invariant: `endCommit.timestamp === info.updated`',
+      );
+    }
+
+    // Not always true for a valid viz.
+    // if (endCommit.parent !== startCommit.id) {
+    //   throw new Error(
+    //     'Invariant: `endCommit.parent === startCommit.id`',
+    //   );
+    // }
+
     // TODO access owner user
     // TODO access start commit and end commit
-    // TODO test Invariant: `startCommit.timestamp === info.created`
-    // TODO test Invariant: `endCommit.timestamp === info.updated`
+    const contentAtEndResult = await getContentAtCommit(
+      info.end,
+    );
+    if (contentAtEndResult.outcome === 'failure') {
+      return contentAtEndResult;
+    }
+    // console.log(JSON.stringify(contentAtEndResult.value));
+    // console.log(JSON.stringify(content));
+    if (
+      // JSON.stringify(contentAtEndResult.value) !==
+      // JSON.stringify(content)
+      !equal(contentAtEndResult.value, content)
+    ) {
+      throw new Error(
+        'Invariant: `contentAtEnd === content`',
+      );
+    }
 
     return ok('success');
   };
