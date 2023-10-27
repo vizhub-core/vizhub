f142a78a3b70359de20d5395b6c855af7ba9ba33
Curran <curran.kelleher@gmail.com>
2023-10-27 14:20:27 -0400

    Make npm link work in dev for vzcode

--- app/vite.config.js
+++ app/vite.config.js
@@ -21,7 +21,15 @@ export default defineConfig({
   // Support Rollup v4
   // See https://github.com/curran/rollup-v4-browser-vite-demo/
   optimizeDeps: {
-    exclude: ['@rollup/browser'],
+    exclude: [
+      // Exclude Rollup v4 from being bundled,
+      // because it messes up the WASM part of the build.
+      '@rollup/browser',
+
+      // Exclude VZCode, so that we can use `npm link` locally
+      // and still have it work with Vite.
+      'vzcode',
+    ],
 
     // Fixes "React is undefined" error with VZCode imports
     // See https://github.com/vitejs/vite-plugin-react/issues/192#issuecomment-1627384670
