d3a7344f278042b5e5a38c5a1eb2c96a57fab1bc
Curran <curran.kelleher@gmail.com>
2023-10-28 17:18:24 -0400

    Simplify

--- components/src/components/LandingPageBody/index.tsx
+++ components/src/components/LandingPageBody/index.tsx
@@ -3,87 +3,64 @@ import { Button } from '../bootstrap';
 import { featureData } from './featureData';
 import './styles.scss';
 
-const Content = ({ feature }) => {
-  return (
-    <div className="feature__group">
-      <div className="feature__top">
-        <div className="feature__header">
-          {feature.sectionHeader}
-        </div>
-        <div className="feature__title">
-          {feature.title}
-        </div>
+const Hero = () => (
+  <div className="hero">
+    <div className="hero__content">
+      <div className="hero__title">
+        See Data Come to Life
       </div>
-      <div className="feature__description">
-        {feature.description}
+      <div className="hero__desc">
+        Discover VizHub, the ultimate platform for creating,
+        sharing, and exploring dynamic data visualizations.
       </div>
-      <Button>{feature.cta}</Button>
+      <Button>Explore Now</Button>
     </div>
-  );
-};
+  </div>
+);
 
-const ImageSection = ({ feature }) => {
-  return (
-    <div className="feature__image">
-      {feature.imageDescription}
-    </div>
-  );
-};
-
-// Put the image
-// Who can use it
-// Title on features section
-
-const HeroSection = () => {
-  return (
-    <div className="vh-landing-page-hero">
-      <div className="vh-landing-page-hero__content">
-        <div className="vh-landing-page-hero__title">
-          See Data Come to Life
-        </div>
-        <div className="vh-landing-page-hero__description">
-          Discover VizHub, the ultimate platform for
-          creating, sharing, and exploring dynamic data
-          visualizations.
-        </div>
-        <Button>Explore Now</Button>
+const Content = ({ feature }) => (
+  <div className="item__group">
+    <div className="item__top">
+      <div className="item__header">
+        {feature.sectionHeader}
       </div>
-      {/* <div className="vh-landing-page-hero__image">
-        <img src="https://user-images.githubusercontent.com/68416/278831147-b5ad6780-1321-4184-aab6-6f3dd83364a6.png" />
-      </div> */}
+      <div className="item__title">{feature.title}</div>
     </div>
-  );
-};
+    <div className="item__desc">{feature.description}</div>
+    <Button>{feature.cta}</Button>
+  </div>
+);
 
-const FeaturesSection = () => {
-  return (
-    <div className="vh-landing-page-features">
-      <div className="vh-landing-page-features__title">
-        FEATURES
+const Image = ({ feature }) => (
+  <div className="item__img">
+    {feature.imageDescription}
+  </div>
+);
+
+const Features = () => (
+  <div className="features">
+    <div className="features__title">FEATURES</div>
+    {featureData.map((feature, index) => (
+      <div key={feature.id || index} className="item">
+        {index % 2 === 0 ? (
+          <>
+            <Content feature={feature} />
+            <Image feature={feature} />
+          </>
+        ) : (
+          <>
+            <Image feature={feature} />
+            <Content feature={feature} />
+          </>
+        )}
       </div>
-      {featureData.map((feature, index) => (
-        <div className="feature">
-          {index % 2 === 0 ? (
-            <>
-              <Content feature={feature} />
-              <ImageSection feature={feature} />
-            </>
-          ) : (
-            <>
-              <ImageSection feature={feature} />
-              <Content feature={feature} />
-            </>
-          )}
-        </div>
-      ))}
-    </div>
-  );
-};
-export const LandingPageBody = () => {
-  return (
-    <div className="vh-page vh-landing-page-body">
-      <HeroSection />
-      <FeaturesSection />
-    </div>
-  );
-};
+    ))}
+  </div>
+);
+
+export const LandingPageBody = () => (
+  <div className="vh-page vh-landing-page-body">
+    <Hero />
+    <Features />
+  </div>
+);
