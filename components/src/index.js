846816c4986c2ccc9f9a05265c3a082e37822fa6
Nita <nitanagdeote1@gmail.com>
2023-11-10 23:14:05 -0500

    remove about and resources

--- components/src/index.js
+++ components/src/index.js
@@ -9,7 +9,5 @@ export { ExplorePageBody } from './components/ExplorePageBody';
 export { ForksPageBody } from './components/ForksPageBody';
 export { VizPageViewer } from './components/VizPageViewer';
 export { UpvoteWidget } from './components/UpvoteWidget';
-export { AboutPageBody } from './components/AboutPageBody';
-export { ResourcesPageBody } from './components/ResourcesPageBody';
 
 import './components/index.scss';
