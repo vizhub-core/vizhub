31d705e1bedf490f8695c9e85bdc1ef7923ffe30
Curran <curran.kelleher@gmail.com>
2023-12-15 13:44:30 -0500

    Implement full pricing page

--- components/src/components/PricingPageBody/index.tsx
+++ components/src/components/PricingPageBody/index.tsx
@@ -1,66 +1,20 @@
 import { useCallback, useState } from 'react';
 import { GreenCheckSVG } from '../Icons/sam/GreenCheckSVG';
 import { Button, ButtonGroup } from '../bootstrap';
-import './styles.scss';
 import { ChevronDownSVG } from '../Icons/sam/ChevronDownSVG';
 import { ChevronUpSVG } from '../Icons/sam/ChevronUpSVG';
+import './styles.scss';
 
 const headerBackgroundSrc =
   'https://gist.github.com/assets/68416/5c51a6f3-2665-4117-82c7-069b089deaca.png';
 
-// function Plans() {
-//   return (
-//     <div className="plans-container">
-//       <h1 className="plans-title">VizHub Plans</h1>
-//       <div className="plans">
-//         <div className="plan">
-//           <div className="plan-header">
-//             <h2 className="plan-title">Starter</h2>
-//             <p className="plan-description">
-//               Lorem ipsum dolor sit amet, consectetur
-//               adipiscing elit.
-//             </p>
-//             <button className="btn current">Current</button>
-//           </div>
-//           <ul className="plan-features">
-//             <li>Public Vizzes</li>
-//             <li>Export Code</li>
-//             <li>Real-time Collaboration</li>
-//             <li>Unfurling</li>
-//           </ul>
-//         </div>
-//         <div className="plan">
-//           <div className="plan-header">
-//             <h2 className="plan-title">Premium</h2>
-//             <p className="plan-description">
-//               Maecenas consequat sagittis orci quis
-//               eleifend.
-//             </p>
-//             <div className="plan-price">$10 / mo</div>
-//             <button className="btn upgrade">Upgrade</button>
-//           </div>
-//           <ul className="plan-features">
-//             <li>Private Vizzes</li>
-//             <li>Public Vizzes</li>
-//             <li>Export Code</li>
-//             <li>Real-time Collaboration</li>
-//             <li>Unfurling</li>
-//           </ul>
-//         </div>
-//       </div>
-//       <div className="plan-switch">
-//         <button className="btn switch">Monthly Plan</button>
-//         <button className="btn switch">Annual Plan</button>
-//       </div>
-//     </div>
-//   );
-// }
 const Feature = ({
   title,
   description,
   hasBottomBorder,
+  startsExpanded = false,
 }) => {
-  const [isOpen, setIsOpen] = useState(false);
+  const [isOpen, setIsOpen] = useState(startsExpanded);
 
   const toggleIsOpen = useCallback(() => {
     setIsOpen((isOpen) => !isOpen);
@@ -120,7 +74,9 @@ export const PricingPageBody = ({
                 <h3 className="plan-header-left">
                   Starter
                 </h3>
-                <h3 className="plan-header-right">Free</h3>
+                <div className="plan-header-right">
+                  <h3>Free</h3>
+                </div>
               </div>
 
               <p>Free forever. Ideal for students.</p>
@@ -139,7 +95,7 @@ export const PricingPageBody = ({
                 />
                 <Feature
                   title="Export Code"
-                  description="Export industry-standard code files for easy integration into existing codebases."
+                  description="Export code for easy integration into existing codebases."
                   hasBottomBorder={true}
                 />
                 <Feature
@@ -155,24 +111,36 @@ export const PricingPageBody = ({
               </div>
             </div>
             <div className="pricing-page-plan">
-              <h3>Premium</h3>
+              <div className="plan-header">
+                <h3 className="plan-header-left">
+                  Premium
+                </h3>
+                <div className="plan-header-right">
+                  <h3>$12</h3>
+                  <h3 className="plan-header-right-faint">
+                    /mo
+                  </h3>
+                </div>
+              </div>
+
               <p>
-                $10 / mo. For individuals and small teams.
+                Ideal for freelancers and professionals.
               </p>
               <Button
                 variant="primary"
-                onClick={onProClick}
                 className="pricing-page-plan-button"
+                onClick={onProClick}
               >
                 Upgrade
               </Button>
-              <ul>
-                <li>Private Vizzes</li>
-                <li>Public Vizzes</li>
-                <li>Export Code</li>
-                <li>Real-time Collaboration</li>
-                <li>Unfurling</li>
-              </ul>
+              <div className="pricing-page-plan-features">
+                <Feature
+                  title="Private Vizzes"
+                  description="Develop visualizations accessible only by you and your collaborators."
+                  hasBottomBorder={false}
+                  startsExpanded={true}
+                />
+              </div>
             </div>
           </div>
         </div>
