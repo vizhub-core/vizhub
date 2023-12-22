56aad5a775ad5ba294ee8db522d3fb45bfb3fab1
Curran <curran.kelleher@gmail.com>
2023-12-21 21:16:51 -0500

    Iterate migration

--- migration3/migrate_loop.sh
+++ migration3/migrate_loop.sh
@@ -1,7 +1,26 @@
+# #!/bin/bash
+# # This script runs the migration script over and over.
+# # This is done so that the database connection is not lost.
+# while true; do
+#   npm run migrate
+#   sleep 5  # Waits for 5 seconds before next run
+# done
+
 #!/bin/bash
 # This script runs the migration script over and over.
-# This is done so that the database connection is not lost.
+# The loop stops if the migration script returns a nonzero exit code.
+
 while true; do
   npm run migrate
+
+  # Capture the exit code of the npm command
+  exit_code=$?
+
+  # Check if the exit code is nonzero (indicating an error)
+  if [ $exit_code -ne 0 ]; then
+    echo "Migration script failed with exit code $exit_code. Exiting loop."
+    break
+  fi
+
   sleep 5  # Waits for 5 seconds before next run
 done
