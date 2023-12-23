b1a04223f63151e13221f155a2d0548d0daaf0f0
Curran <curran.kelleher@gmail.com>
2023-12-23 09:59:08 -0500

    Use Redis URL from ENV

--- database/src/initializeRedis.ts
+++ database/src/initializeRedis.ts
@@ -1,9 +1,9 @@
 import redis from 'redis';
 export const initializeRedis = async () => {
   const redisClient = redis.createClient({
-    // url:
-    //   process.env.VIZHUB3_REDIS_URL ||
-    //   'redis://localhost:6379',
+    url:
+      process.env.VIZHUB3_REDIS_URL ||
+      'redis://localhost:6379',
 
     // This makes it work with ShareDB
     // See https://github.com/share/sharedb-redis-pubsub/issues/19
@@ -18,6 +18,7 @@ export const initializeRedis = async () => {
 
   // Validate the connection
   await new Promise((resolve, reject) => {
+    // @ts-ignore
     redisClient.ping((err, result) => {
       if (err) {
         reject(err);
