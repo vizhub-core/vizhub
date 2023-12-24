28cb7e6cf877b040dc179cf05da9b10a637c1c56
Curran <curran.kelleher@gmail.com>
2023-12-24 10:17:39 -0500

    Attempt fix

--- database/src/initializeRedlock.ts
+++ database/src/initializeRedlock.ts
@@ -1,25 +1,28 @@
-// import Client from 'ioredis';
+import Client from 'ioredis';
 import Redlock from 'redlock';
-// import { redisURL } from './initializeRedis';
+import { redisURL } from './initializeRedis';
 
-export const initializeRedlock = async (redisClient) => {
-  // const ioRedisClient = new Client(redisURL);
+export const initializeRedlock = async () => {
+  const ioRedisClient = new Client(redisURL);
 
   console.log(
     '  Connecting to Redis for redlock via ioredis...',
   );
 
-  // // Perform a ping check to ensure Redis connection is active
-  // try {
-  //   const pingResponse = await ioRedisClient.ping();
-  //   console.log('    Redis ping response:', pingResponse); // Should log "PONG" if successful
-  // } catch (error) {
-  //   console.error('    Failed to ping Redis:', error);
-  //   throw error; // Optionally re-throw the error to handle it outside this function
-  // }
+  // Perform a ping check to ensure Redis connection is active
+  try {
+    const pingResponse = await ioRedisClient.ping();
+    console.log('    Redis ping response:', pingResponse); // Should log "PONG" if successful
+    if (pingResponse !== 'PONG') {
+      throw new Error('Redis ping response was not PONG');
+    }
+  } catch (error) {
+    console.error('    Failed to ping Redis:', error);
+    throw error; // Optionally re-throw the error to handle it outside this function
+  }
 
   // See https://github.com/mike-marcacci/node-redlock?tab=readme-ov-file#configuration
-  const redlock = new Redlock([redisClient], {
+  const redlock = new Redlock([ioRedisClient], {
     // The expected clock drift; for more details see:
     // http://redis.io/topics/distlock
     driftFactor: 0.01, // multiplied by lock ttl to determine drift time
