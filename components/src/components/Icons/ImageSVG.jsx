cfc730747206d27a322e170325df5427ee5474c4
Curran <curran.kelleher@gmail.com>
2024-02-03 18:17:15 -0500

    Upgrade deps, fix SVG bug

--- components/src/components/Icons/ImageSVG.jsx
+++ components/src/components/Icons/ImageSVG.jsx
@@ -5,7 +5,7 @@ export const ImageSVG = ({ height = 24 }) => (
     xmlns="http://www.w3.org/2000/svg"
     fill="none"
     viewBox="0 0 24 24"
-    stroke-width="1.5"
+    strokeWidth="1.5"
     stroke="currentColor"
   >
     <path
