f7c4ef93c7b6d76b96c5bd3aa00faf70c75f8d2e
Curran <curran.kelleher@gmail.com>
2024-02-04 10:12:05 -0500

    Update icons and styles for viz head

---
+++ components/src/components/Icons/sam/ArrowRightSVG.tsx
@@ -0,0 +1,18 @@
+export const ArrowRightSVG = () => (
+  <svg
+    width="24"
+    height="24"
+    xmlns="http://www.w3.org/2000/svg"
+    fill="none"
+    viewBox="0 0 24 24"
+    strokeWidth={1.5}
+    stroke="currentColor"
+    className="w-6 h-6"
+  >
+    <path
+      strokeLinecap="round"
+      strokeLinejoin="round"
+      d="M13.5 4.5 21 12m0 0-7.5 7.5M21 12H3"
+    />
+  </svg>
+);
