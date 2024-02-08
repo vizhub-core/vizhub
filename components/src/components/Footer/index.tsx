930d49cd56687cdd4f7de8d1fed091bec5a1a3c9
Curran <curran.kelleher@gmail.com>
2024-02-08 12:52:58 -0500

    Clean up

--- components/src/components/Footer/index.tsx
+++ components/src/components/Footer/index.tsx
@@ -1,8 +1,11 @@
 import { LogoSVG } from '../Icons/LogoSVG';
 import { FacebookSVG } from '../Icons/FacebookSVG';
-
 import { discordLink } from '../discordLink';
 import './styles.scss';
+
+// TODO enable this once styling is worked out
+const enableSocialIcons = false;
+
 export const Footer = () => {
   return (
     <div className="vh-footer">
@@ -10,9 +13,27 @@ export const Footer = () => {
         <LogoSVG height={50} />
         <div className="footer-links">
           <div className="footer-links-title">Platform</div>
-          <a href="/pricing">Pricing</a>
-          <a href="/explore">Explore</a>
-          <a href="/create-Viz">Create</a>
+          <a
+            href="/pricing"
+            target="_blank"
+            rel="noreferrer"
+          >
+            Pricing
+          </a>
+          <a
+            href="/explore"
+            target="_blank"
+            rel="noreferrer"
+          >
+            Explore
+          </a>
+          <a
+            href="/create-Viz"
+            target="_blank"
+            rel="noreferrer"
+          >
+            Create
+          </a>
 
           {/* <a href="/terms">Terms</a> */}
         </div>
@@ -20,7 +41,11 @@ export const Footer = () => {
           <div className="footer-links-title">Contact</div>
 
           <a href="mailto:contact@vizhub.com">Email</a>
-          <a href="https://calendly.com/curran-kelleher/data-visualization-consultation">
+          <a
+            href="https://calendly.com/curran-kelleher/data-visualization-consultation"
+            target="_blank"
+            rel="noreferrer"
+          >
             Consulting
           </a>
         </div>
@@ -29,23 +54,42 @@ export const Footer = () => {
           <div className="footer-links-title">
             <span>Community</span>
           </div>
-          <a href={discordLink}>Discord</a>
-          <a href="/forum">Forum</a>
+          <a
+            href={discordLink}
+            target="_blank"
+            rel="noreferrer"
+          >
+            Discord
+          </a>
+          <a href="/forum" target="_blank" rel="noreferrer">
+            Forum
+          </a>
         </div>
 
         <div className="footer-links">
           <div className="footer-links-title">
             <span>Follow us</span>
           </div>
-          <a href="https://twitter.com/viz_hub">Twitter</a>
-          <a href="https://www.youtube.com/@currankelleher">
+          <a
+            href="https://twitter.com/viz_hub"
+            target="_blank"
+            rel="noreferrer"
+          >
+            Twitter
+          </a>
+          <a
+            href="https://www.youtube.com/@currankelleher"
+            target="_blank"
+            rel="noreferrer"
+          >
             YouTube
           </a>
           <a
-            href="/https://www.facebook.com/profile.php?id=100071381815409"
+            href="https://www.facebook.com/people/VizHub/100071381815409/"
             target="_blank"
+            rel="noreferrer"
           >
-            <FacebookSVG />
+            {enableSocialIcons && <FacebookSVG />}
             <span>Facebook</span>
           </a>
         </div>
