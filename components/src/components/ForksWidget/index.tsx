66e10c8f8d32812f9711bc0c3ed6b0578ce909aa
Curran <curran.kelleher@gmail.com>
2024-02-17 09:34:03 -0500

    Iterate forks count format

--- components/src/components/ForksWidget/index.tsx
+++ components/src/components/ForksWidget/index.tsx
@@ -1,5 +1,7 @@
+import { useMemo } from 'react';
 import { ForkSVG } from '../Icons/sam/ForkSVG';
 import { OverlayTrigger, Tooltip } from '../bootstrap';
+import { commaFormat } from '../commaFormat';
 import './styles.scss';
 
 export const ForksWidget = ({
@@ -8,58 +10,64 @@ export const ForksWidget = ({
   onClick = null,
   notClickable = false,
   isUserAuthenticated = false,
-}) => (
-  <div
-    className={`vh-forks-widget${notClickable ? ' not-clickable' : ''}`}
-  >
-    <OverlayTrigger
-      placement="top"
-      overlay={
-        <Tooltip id="full-screen-icon-tooltip">
-          {isUserAuthenticated
-            ? 'Fork this viz'
-            : 'Log in to fork this viz'}
-        </Tooltip>
-      }
+}) => {
+  const forksCountFormatted = useMemo(
+    () => commaFormat(forksCount),
+    [forksCount],
+  );
+  return (
+    <div
+      className={`vh-forks-widget${notClickable ? ' not-clickable' : ''}`}
     >
-      <i
-        className="icon-button icon-button-light"
-        onClick={isUserAuthenticated ? onClick : null}
-        style={{
-          cursor: isUserAuthenticated
-            ? 'pointer'
-            : 'not-allowed',
-        }}
+      <OverlayTrigger
+        placement="top"
+        overlay={
+          <Tooltip id="full-screen-icon-tooltip">
+            {isUserAuthenticated
+              ? 'Fork this viz'
+              : 'Log in to fork this viz'}
+          </Tooltip>
+        }
       >
-        <ForkSVG />
-      </i>
-    </OverlayTrigger>
-    <OverlayTrigger
-      placement="top"
-      overlay={
-        <Tooltip id="full-screen-icon-tooltip">
-          View Forks
-        </Tooltip>
-      }
-    >
-      <a
-        className="icon-button icon-button-light"
-        href={forksPageHref}
-        target="_blank"
-        rel="noopener noreferrer"
+        <i
+          className="icon-button icon-button-light"
+          onClick={isUserAuthenticated ? onClick : null}
+          style={{
+            cursor: isUserAuthenticated
+              ? 'pointer'
+              : 'not-allowed',
+          }}
+        >
+          <ForkSVG />
+        </i>
+      </OverlayTrigger>
+      <OverlayTrigger
+        placement="top"
+        overlay={
+          <Tooltip id="full-screen-icon-tooltip">
+            View Forks
+          </Tooltip>
+        }
       >
-        <strong>{forksCount}</strong>
-        <div className="widget-label">
-          Fork{forksCount === 1 ? '' : 's'}
-        </div>
-      </a>
-    </OverlayTrigger>
-  </div>
-);
+        <a
+          className="icon-button icon-button-light"
+          href={forksPageHref}
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <strong>{forksCountFormatted}</strong>
+          <div className="widget-label">
+            Fork{forksCount === 1 ? '' : 's'}
+          </div>
+        </a>
+      </OverlayTrigger>
+    </div>
+  );
+};
 
 // import { useMemo } from 'react';
 // import { ForkSVG } from '../Icons/sam/ForkSVG';
-// import { commaFormat } from '../commaFormat';
+
 // import './styles.scss';
 
 // const enableForksWidget = true;
