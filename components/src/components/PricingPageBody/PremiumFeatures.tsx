d281217a2f5b5a126e607b259d5a512dcf9264ff
Curran <curran.kelleher@gmail.com>
2024-02-29 13:28:06 -0500

    Iterate pricing page

--- components/src/components/PricingPageBody/PremiumFeatures.tsx
+++ components/src/components/PricingPageBody/PremiumFeatures.tsx
@@ -4,29 +4,35 @@ import { Feature } from './Feature';
 export const PremiumFeatures = ({}) => (
   <>
     <Feature title="Private Vizzes" hasBottomBorder={true}>
-      Make your vizzes private
-    </Feature>
-    <Feature
-      title="Unlimited Real-Time Collaborators"
-      hasBottomBorder={true}
-    >
-      Invite friends and colleagues to view and edit your
-      vizzes
-    </Feature>
-    <Feature
-      title="White Label Embedding"
-      hasBottomBorder={true}
-    >
-      Embed interactive vizzes without attribution
+      Maintain confidentiality with private visualizations
     </Feature>
     <Feature
       title="AI-Assisted Coding"
       hasBottomBorder={true}
       learnMoreHref="https://vizhub.com/forum/t/ai-assisted-coding/952"
-      startsExpanded={false}
     >
-      On-demand coding assistance from artificial
+      Speed up your coding process with artificial
       intelligence
+    </Feature>
+    <Feature
+      title="Unlimited Real-Time Collaborators"
+      hasBottomBorder={true}
+      learnMoreHref={
+        'https://vizhub.com/forum/t/real-time-collaborators/976'
+      }
+    >
+      Share your work selectively with specific
+      collaborators
+    </Feature>
+    <Feature
+      title="White Label Embedding"
+      hasBottomBorder={true}
+      learnMoreHref={
+        'https://vizhub.com/forum/t/embedding-vizzes/975'
+      }
+    >
+      Embed interactive visualizations without VizHub
+      branding
     </Feature>
     <Feature title="Upload Larger Datasets">
       Data uploads are limited to {premiumTierSizeLimitMB}{' '}
