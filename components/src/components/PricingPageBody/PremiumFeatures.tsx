c3336c9b8785e66e3450d1e3db63c00d114675a4
Curran <curran.kelleher@gmail.com>
2024-03-13 17:06:33 -0400

    Make collaborators free

--- components/src/components/PricingPageBody/PremiumFeatures.tsx
+++ components/src/components/PricingPageBody/PremiumFeatures.tsx
@@ -17,31 +17,6 @@ export const PremiumFeatures = ({}) => (
       settings.
     </Feature>
     <Feature
-      title="AI-Assisted Coding"
-      id="ai-assisted-coding"
-      hasBottomBorder={true}
-      learnMoreHref="https://vizhub.com/forum/t/ai-assisted-coding/952"
-    >
-      Enhance your coding with AI. Speed up your workflow,
-      reduce errors, and innovate faster. Break through
-      technical challenges faster, freeing up time for
-      research, analysis, and creativity.
-    </Feature>
-    <Feature
-      title="Unlimited Real-Time Collaborators"
-      id="real-time-collaborators"
-      hasBottomBorder={true}
-      learnMoreHref={
-        'https://vizhub.com/forum/t/real-time-collaborators/976'
-      }
-    >
-      Break down barriers to collaboration. Share your
-      projects with an unlimited number of collaborators for
-      a truly integrated team experience. Perfect for remote
-      pair programming, live coding interviews, and
-      actioning client feedback with the client on the call.
-    </Feature>
-    <Feature
       title="White Label Embedding"
       id="white-label-embedding"
       hasBottomBorder={true}
@@ -53,6 +28,17 @@ export const PremiumFeatures = ({}) => (
       visualizations seamlessly into your Web pages or
       application, without the VizHub logo and linkback. Let
       VizHub host your interactives so you don't have to.
+    </Feature>
+    <Feature
+      title="AI-Assisted Coding"
+      id="ai-assisted-coding"
+      hasBottomBorder={true}
+      learnMoreHref="https://vizhub.com/forum/t/ai-assisted-coding/952"
+    >
+      Enhance your coding with AI. Speed up your workflow,
+      reduce errors, and innovate faster. Break through
+      technical challenges faster, freeing up time for
+      research, analysis, and creativity.
     </Feature>
     <Feature
       title="Upload Larger Datasets"
