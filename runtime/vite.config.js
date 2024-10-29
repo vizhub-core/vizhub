984d2824b9267e7f07f305fe95b742d7da099636
Curran <curran.kelleher@gmail.com>
2024-10-29 00:18:20 -0400

    Attempt export types from NPM package

--- runtime/vite.config.js
+++ runtime/vite.config.js
@@ -1,22 +1,12 @@
-// Inspired by https://github.com/vitejs/vite-plugin-react/blob/main/playground/ssr-react/vite.config.js
 import { defineConfig } from 'vite';
-// import react from '@vitejs/plugin-react';
-// import { visualizer } from 'rollup-plugin-visualizer';
 
 export default defineConfig({
-  // plugins: [react()],
   optimizeDeps: {
     exclude: [
       // Exclude Rollup v4 from being bundled,
       // because it messes up the WASM part of the build.
       '@rollup/browser',
     ],
-
-    // Fixes "React is undefined" error with VZCode imports
-    // See https://github.com/vitejs/vite-plugin-react/issues/192#issuecomment-1627384670
-    esbuildOptions: {
-      jsx: 'automatic',
-    },
   },
   // Fix CSS warnings
   css: {
