c83ab9304d7b51cbd5f3512b44d7df20b9500dd6
Curran <curran.kelleher@gmail.com>
2025-02-18 15:10:01 -0500

    Fix CI

--- app/vite.config.js
+++ app/vite.config.js
@@ -18,11 +18,46 @@ export default defineConfig({
         __dirname,
         '../runtime/src',
       ),
+
+      /**
+       * @uiw/* packages are not compatible with ESM, as they do not import with `.js` extension.
+       * We're using source in TypeScript instead.
+       */
+
+      '@uiw/codemirror-theme-okaidia':
+        '@uiw/codemirror-theme-okaidia/src/index.ts',
+      '@uiw/codemirror-theme-abcdef':
+        '@uiw/codemirror-theme-abcdef/src/index.ts',
+      '@uiw/codemirror-theme-dracula':
+        '@uiw/codemirror-theme-dracula/src/index.ts',
+      '@uiw/codemirror-theme-eclipse':
+        '@uiw/codemirror-theme-eclipse/src/index.ts',
+      '@uiw/codemirror-theme-github':
+        '@uiw/codemirror-theme-github/src/index.ts',
+      '@uiw/codemirror-theme-material':
+        '@uiw/codemirror-theme-material/src/index.ts',
+      '@uiw/codemirror-theme-nord':
+        '@uiw/codemirror-theme-nord/src/index.ts',
+      '@uiw/codemirror-theme-xcode':
+        '@uiw/codemirror-theme-xcode/src/index.ts',
+      '@uiw/codemirror-themes':
+        '@uiw/codemirror-themes/src/index.tsx',
     },
   },
   // Support Rollup v4
   // See https://github.com/curran/rollup-v4-browser-vite-demo/
   optimizeDeps: {
+    include: [
+      '@uiw/codemirror-theme-okaidia',
+      '@uiw/codemirror-theme-abcdef',
+      '@uiw/codemirror-theme-dracula',
+      '@uiw/codemirror-theme-eclipse',
+      '@uiw/codemirror-theme-github',
+      '@uiw/codemirror-theme-material',
+      '@uiw/codemirror-theme-nord',
+      '@uiw/codemirror-theme-xcode',
+      '@uiw/codemirror-themes',
+    ],
     exclude: [
       // Exclude Rollup v4 from being bundled,
       // because it messes up the WASM part of the build.
