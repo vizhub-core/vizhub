6cac03bc8bdda8aba6370b305900d8edd52f0896
Curran <curran.kelleher@gmail.com>
2025-04-13 12:52:16 -0400

    Slight progress

--- app/src/pages/VizPage/runtime/useRuntime.ts
+++ app/src/pages/VizPage/runtime/useRuntime.ts
@@ -1,4 +1,9 @@
-import { RefObject, useEffect, useRef } from 'react';
+import {
+  RefObject,
+  useCallback,
+  useEffect,
+  useRef,
+} from 'react';
 import { VizContent, VizId } from '@vizhub/viz-types';
 import { createRuntime } from '@vizhub/runtime';
 import type { VizHubRuntime } from '@vizhub/runtime';
@@ -35,75 +40,63 @@ export const useRuntime = ({
   // // This ref is used to skip the first mount.
   const initialMount = useRef(true);
 
-  const vizHubRuntimeRef = useRef<VizHubRuntime | null>(
-    null,
+  const runtimeRef = useRef<VizHubRuntime | null>(null);
+
+  // Poll for this to be defined.
+  const getVizHubRuntime = useCallback(async () => {
+    if (runtimeRef.current === null) {
+      return new Promise<VizHubRuntime>((resolve) => {
+        const interval = setInterval(() => {
+          if (debug) {
+            console.log(
+              'polling for VizHubRuntime.current...',
+            );
+          }
+          if (runtimeRef.current !== null) {
+            clearInterval(interval);
+            resolve(runtimeRef.current);
+          }
+        }, 100);
+      });
+    }
+    return runtimeRef.current;
+  }, []);
+
+  const vizCacheContentsRef = useRef(vizCacheContents);
+
+  useEffect(() => {
+    vizCacheContentsRef.current = vizCacheContents;
+  }, [vizCacheContents]);
+
+  // Handles cache misses for viz content,
+  // when a viz imports from another viz.
+  const getLatestContent = useCallback(
+    async (vizId: VizId): Promise<VizContent> => {
+      // Sanity check, should never happen.
+      if (!vizCacheContentsRef.current) {
+        throw new Error(
+          'vizCacheContentsRef.current is null',
+        );
+      }
+
+      const content = vizCacheContentsRef.current[vizId];
+
+      // If the viz content for this import is already tracked,
+      // then return it.
+      if (content) {
+        return content;
+      } else {
+        // TODO make this happen by:
+        // * Fetching the viz content from the server
+        // * Using a new API endpoint that returns the viz content
+        // * Ingesting the snapshot and incorporating it into the vizCacheContents
+        throw new Error(
+          `TODO client-side fetching of newly imported vizzes. Current workaround: refresh the page`,
+        );
+      }
+    },
+    [],
   );
-
-  // const getVizHubRuntime = useCallback(async () => {
-  //   if (VizHubRuntimeRef.current === null) {
-  //     // throw new Error('VizHubRuntime.current is null');
-  //     // Poll for this to be defined.
-  //     // const interval = setInterval(() => {
-  //     //   if (debug) {
-  //     //     console.log('polling for VizHubRuntime.current...');
-  //     //   }
-  //     //   if (VizHubRuntimeRef.current !== null) {
-  //     //     clearInterval(interval);
-  //     //     return VizHubRuntimeRef.current;
-  //     //   }
-  //     // }, 100);
-  //     return new Promise<VizHubRuntime>((resolve) => {
-  //       const interval = setInterval(() => {
-  //         if (debug) {
-  //           console.log(
-  //             'polling for VizHubRuntime.current...',
-  //           );
-  //         }
-  //         if (VizHubRuntimeRef.current !== null) {
-  //           clearInterval(interval);
-  //           resolve(VizHubRuntimeRef.current);
-  //         }
-  //       }, 100);
-  //     });
-  //   }
-  //   return VizHubRuntimeRef.current;
-  // }, []);
-
-  // const vizCacheContentsRef = useRef(vizCacheContents);
-
-  // useEffect(() => {
-  //   vizCacheContentsRef.current = vizCacheContents;
-  // }, [vizCacheContents]);
-
-  // // Handles cache misses for viz content,
-  // // when a viz imports from another viz.
-  // const getLatestContent = useCallback(
-  //   async (vizId: VizId): Promise<VizContent> => {
-  //     // Sanity check, should never happen.
-  //     if (!vizCacheContentsRef.current) {
-  //       throw new Error(
-  //         'vizCacheContentsRef.current is null',
-  //       );
-  //     }
-
-  //     const content = vizCacheContentsRef.current[vizId];
-
-  //     // If the viz content for this import is already tracked,
-  //     // then return it.
-  //     if (content) {
-  //       return content;
-  //     } else {
-  //       // TODO make this happen by:
-  //       // * Fetching the viz content from the server
-  //       // * Using a new API endpoint that returns the viz content
-  //       // * Ingesting the snapshot and incorporating it into the vizCacheContents
-  //       throw new Error(
-  //         `TODO client-side fetching of newly imported vizzes. Current workaround: refresh the page`,
-  //       );
-  //     }
-  //   },
-  //   [],
-  // );
 
   useEffect(() => {
     // If the viz is not visual (README.md only), then
@@ -170,7 +163,7 @@ export const useRuntime = ({
 
     console.log('Calling createRuntime');
     const worker = new BuildWorker();
-    vizHubRuntimeRef.current = createRuntime({
+    runtimeRef.current = createRuntime({
       iframe,
       setBuildErrorMessage: setSrcdocErrorMessage,
       worker,
@@ -186,105 +179,102 @@ export const useRuntime = ({
   }, [isVisual]);
 
   // Send updates of imported vizzes to the V3 runtime.
-  // const previousVizCacheContents = useRef(vizCacheContents);
+  const previousVizCacheContents = useRef(vizCacheContents);
 
   // Track the srcdoc error message, but only check it
   // when we are attempting to run (do not re-run when
   // the error message changes or is cleared).
-  // const srcdocErrorMessageRef = useRef(srcdocErrorMessage);
-  // useEffect(() => {
-  //   srcdocErrorMessageRef.current = srcdocErrorMessage;
-  // }, [srcdocErrorMessage]);
+  const srcdocErrorMessageRef = useRef(srcdocErrorMessage);
+  useEffect(() => {
+    srcdocErrorMessageRef.current = srcdocErrorMessage;
+  }, [srcdocErrorMessage]);
 
-  // useEffect(() => {
-  //   // // Don't crash for v2 runtime!
-  //   // if (runtimeVersion !== 3) {
-  //   //   return;
-  //   // }
+  useEffect(() => {
+    console.log(
+      'vizCacheContents updated',
+      vizCacheContents,
+    );
 
-  //   // Don't run on first render.
-  //   if (initialMount.current === true) {
-  //     return;
-  //   }
+    // Don't run on first render.
+    if (initialMount.current === true) {
+      return;
+    }
 
-  //   // Don't run if the viz is not visual (README.md only).
-  //   if (isVisual === false) {
-  //     return;
-  //   }
+    // Don't run if the viz is not visual (README.md only).
+    if (isVisual === false) {
+      return;
+    }
 
-  //   // console.log(
-  //   //   'TODO update the v3 runtime when imported vizzes change',
-  //   // );
+    console.log('HERE');
+    // Find the imported vizzes that have changed.
+    const changedVizIds = Object.keys(
+      vizCacheContents,
+    ).filter((vizId) => {
+      return (
+        previousVizCacheContents.current[vizId] !==
+        vizCacheContents[vizId]
+      );
+    });
+    previousVizCacheContents.current = vizCacheContents;
 
-  //   // Find the imported vizzes that have changed.
-  //   const changedVizIds = Object.keys(
-  //     vizCacheContents,
-  //   ).filter((vizId) => {
-  //     return (
-  //       previousVizCacheContents.current[vizId] !==
-  //       vizCacheContents[vizId]
-  //     );
-  //   });
-  //   previousVizCacheContents.current = vizCacheContents;
+    if (debug) {
+      console.log(
+        '[useRuntime] changedVizIds',
+        changedVizIds,
+      );
+    }
 
-  //   if (debug) {
-  //     console.log(
-  //       '[useRuntime] changedVizIds',
-  //       changedVizIds,
-  //     );
-  //   }
+    if (changedVizIds.length === 0) {
+      return;
+    }
 
-  //   if (changedVizIds.length === 0) {
-  //     return;
-  //   }
+    // // See if any of the vizzes we import from are interacting.
+    let isInteracting = false;
+    for (const vizId of changedVizIds) {
+      if (vizCacheContents[vizId].isInteracting) {
+        isInteracting = true;
+        break;
+      }
+    }
+    if (debug) {
+      console.log('isInteracting', isInteracting);
+    }
 
-  //   // // See if any of the vizzes we import from are interacting.
-  //   let isInteracting = false;
-  //   for (const vizId of changedVizIds) {
-  //     if (vizCacheContents[vizId].isInteracting) {
-  //       isInteracting = true;
-  //       break;
-  //     }
-  //   }
-  //   if (debug) {
-  //     console.log('isInteracting', isInteracting);
-  //   }
+    const update = async () => {
+      const runtime = await getVizHubRuntime();
+      // Sanity check, should never happen.
+      if (runtime === null) {
+        throw new Error('VizHubRuntime is null');
+      }
+      if (isInteracting) {
+        // If we are recovering from an error,
+        // clear the error message, and run the code
+        // totally fresh by re-computing the srcdoc.
+        if (srcdocErrorMessageRef.current) {
+          runtime.resetSrcdoc(changedVizIds);
+          // try {
+          //   const srcdoc = await computeSrcDocV3(content);
+          //   if (iframeRef.current) {
+          //     iframeRef.current.srcdoc = srcdoc;
+          //   }
+          // } catch (error) {
+          //   console.error(error);
+          //   setSrcdocErrorMessage(error.message);
+          // }
+        } else {
+          // Re-run the code with hot reloading.
+          runtime.invalidateVizCache(changedVizIds);
+        }
+      }
+    };
+    update();
+  }, [vizCacheContents]);
 
-  //   const update = async () => {
-  //     const VizHubRuntime = await getVizHubRuntime();
-  //     // Sanity check, should never happen.
-  //     if (VizHubRuntime === null) {
-  //       throw new Error('VizHubRuntime is null');
-  //     }
-  //     if (isInteracting) {
-  //       // If we are recovering from an error,
-  //       // clear the error message, and run the code
-  //       // totally fresh by re-computing the srcdoc.
-  //       if (srcdocErrorMessageRef.current) {
-  //         VizHubRuntime.resetSrcdoc(changedVizIds);
-  //         // try {
-  //         //   const srcdoc = await computeSrcDocV3(content);
-  //         //   if (iframeRef.current) {
-  //         //     iframeRef.current.srcdoc = srcdoc;
-  //         //   }
-  //         // } catch (error) {
-  //         //   console.error(error);
-  //         //   setSrcdocErrorMessage(error.message);
-  //         // }
-  //       } else {
-  //         // Re-run the code with hot reloading.
-  //         VizHubRuntime.invalidateVizCache(changedVizIds);
-  //       }
-  //     }
-  //   };
-  //   update();
-  // }, [vizCacheContents, runtimeVersion]);
-
-  // // Track the initial mount.
-  // useEffect(() => {
-  //   if (initialMount.current) {
-  //     initialMount.current = false;
-  //     return;
-  //   }
-  // }, []);
+  // Track the initial mount.
+  useEffect(() => {
+    if (initialMount.current) {
+      initialMount.current = false;
+      return;
+    }
+  }, []);
 };
