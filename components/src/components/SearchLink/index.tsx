1f4eedcb0c993f7f12479100e3c68286991359c1
Nita <nitanagdeote1@gmail.com>
2025-07-15 17:22:31 -0400

    modified-search-page

---
+++ components/src/components/SearchLink/index.tsx
@@ -0,0 +1,25 @@
+import { Link } from '../Link';
+import './styles.scss';
+
+export const SearchLink = ({ className = '' }) => {
+  return (
+    <Link href="/search" className={`search-link ${className}`}>
+      <svg 
+        xmlns="http://www.w3.org/2000/svg" 
+        width="16" 
+        height="16" 
+        viewBox="0 0 24 24" 
+        fill="none" 
+        stroke="currentColor" 
+        strokeWidth="2" 
+        strokeLinecap="round" 
+        strokeLinejoin="round"
+        className="search-icon"
+      >
+        <circle cx="11" cy="11" r="8"></circle>
+        <line x1="21" y1="21" x2="16.65" y2="16.65"></line>
+      </svg>
+      <span>Search Visualizations</span>
+    </Link>
+  );
+};
