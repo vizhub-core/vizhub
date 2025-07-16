db502245510ec0c91e164c46f323e5cdaa7df82a
Jack Hankin <jackmhankin@gmail.com>
2025-07-16 11:10:02 -0400

    Added notifications for mentions

--- api/src/endpoints/addCommentEndpoint.tsx
+++ api/src/endpoints/addCommentEndpoint.tsx
@@ -34,6 +34,7 @@ export const addCommentEndpoint = ({
     saveNotification,
     getMergeRequest,
     incrementUserUnreadNotificationsCount,
+    getUserByUserName,
   } = gateways;
 
   const sendCommentEmail = async (
@@ -278,6 +279,42 @@ export const addCommentEndpoint = ({
           incrementUserUnreadNotificationsCount(
             recipientUserId,
           );
+
+        //Create notifications for mentions
+
+        const mentionsRegex = RegExp(
+          '@([a-zA-Z\d](?:[a-zA-Z\d]|-(?=[a-zA-Z\d]))*)',
+          'g',
+        );
+
+        const foundUsernames = [
+          ...comment.markdown.matchAll(mentionsRegex),
+        ];
+
+        foundUsernames.forEach(async (match) => {
+          const userResult = await getUserByUserName(
+            match[1],
+          );
+          if (userResult.outcome == 'success') {
+            const mentionNofificationId = generateVizId();
+
+            const mentionNotification: VizNotification = {
+              id: mentionNofificationId,
+              type: VizNotificationType.Mention,
+              user: userResult.value.data.id,
+              resource: comment.resource,
+              created: comment.created,
+              read: false,
+              commentId: comment.id,
+            };
+            const saveMentionNotificationResult =
+              await saveNotification(mentionNotification);
+            const mentionIncrementResult =
+              incrementUserUnreadNotificationsCount(
+                userResult.value.data.id,
+              );
+          }
+        });
 
         res.send(incrementResult);
       }
