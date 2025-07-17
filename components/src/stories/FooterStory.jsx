d30ff89e271d32692b58b52f7b61bb97ce9d142e
Nita <nitanagdeote1@gmail.com>
2025-07-17 00:42:23 -0400

    improved-Footer

---
+++ components/src/stories/FooterStory.jsx
@@ -0,0 +1,13 @@
+import { Footer } from '../components/Footer';
+
+const Story = () => {
+  return (
+    <div className="layout-fullscreen">
+      <div style={{ minHeight: '80vh', display: 'flex', alignItems: 'flex-end' }}>
+        <Footer />
+      </div>
+    </div>
+  );
+};
+
+export default Story;
