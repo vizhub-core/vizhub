94413cf66b25528170241138152eed560ed45063
Curran Kelleher <68416+curran@users.noreply.github.com>
2025-08-01 16:17:15 -0400

    Update components/src/components/ExplorePageBody/index.tsx

--- components/src/components/ExplorePageBody/index.tsx
+++ components/src/components/ExplorePageBody/index.tsx
@@ -48,7 +48,6 @@ export const ExplorePageBody = ({
             <div className="feature-list">
               <div 
                 className="feature-item"
-                data-bs-toggle="tooltip"
                 data-bs-placement="bottom"
                 title="Create visualizations instantly with AI assistance"
               >
